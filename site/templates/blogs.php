025b7134d024cfd4120fe55d9631208db7856ee3
Carsten Niehaus <gitlab@niehaus-ol.de>
2020-01-13 22:49:40 +0100

    Ganz nette Seite über die Blog

--- site/templates/blogs.php
+++ site/templates/blogs.php
@@ -1,7 +1,77 @@
 <?php snippet('header') ?>
 
-<?php snippet('blogs', [
-  'blogs' => $page->children()->listed()
-  ]) ?>
+<section class="jumbotron text-center">
+  <div class="container">
+    <h1>Die Fächer stellen sich vor</h1>
+
+    <p class="lead">Hier stellen sich die Fächer und Fachbereiche vor. Klicken Sie auf das Symbolbild, um aktuelle Nachrichten zu lesen!</p>
+  </div>
+</section>
+
+
+
+<div class="container">
+
+  <div class="row">
+    <div class="col-md-4">
+      <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/commons/4/41/Sunflower_from_Silesia2.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Biologie</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="https://www.iconspng.com/images/book-coloured/book-coloured.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Deutsch</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="https://www.inforadio.de/content/dam/rbb/inf/Headerbilder_Sendestrecken/SportB-lle_1280.jpg.jpg/rendition=SportBlle_975.jpg.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Sport</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-4">
+
+    </div>
+    <div class="col-md-4">
+      >
+    </div>
+    <div class="col-md-4">
+
+    </div>
+
+    <div class="col-md-4">
+
+    </div>
+    <div class="col-md-4">
+
+    </div>
+    <div class="col-md-4">
+
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
 
 <?php snippet('footer') ?>
