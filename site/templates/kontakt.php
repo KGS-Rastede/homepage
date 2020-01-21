726f964bfa50041b4790dd1677f5482fa57975d1
Carsten Niehaus <ni@kgs-rastede.de>
2020-01-21 15:02:47 +0100

    Und nun nicht mehr manuell erstellen

--- site/templates/kontakt.php
+++ site/templates/kontakt.php
@@ -2,113 +2,24 @@
 
 <h1>Die Schulleitung</h1>
 
-<div class="row row-cols-1 row-cols-md-4">
-    <div class="col mb-4">
-        <div class="card h-100" style="width: 16rem;">
-            <img src="<?= url('assets/fotos_kontakte/be.jpg') ?>" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Schulleiterin</h5>
-                <p class="card-text">Claudia Berger</p>
-            </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">be@kgs-rastede.de</li>
-                <li class="list-group-item">0049-4402-9264-13</li>
-            </ul>
-        </div>
-    </div>
-    <div class="col mb-4">
-        <div class="card h-100" style="width: 16rem;">
-            <img src="<?= url('assets/fotos_kontakte/ni.jpg') ?>" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Direktorstellvertreter</h5>
-                <p class="card-text">Carsten Niehaus</p>
-            </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">ni@kgs-rastede.de</li>
-                <li class="list-group-item">0049-4402-9264-19</li>
-            </ul>
-        </div>
-    </div>
-    <div class="col mb-4">
-        <div class="card h-100" style="width: 16rem;">
-            <img src="<?= url('assets/fotos_kontakte/bo.jpg') ?>" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Gynnasialzweigleiter</h5>
-                <p class="card-text">Malte Bormann</p>
-            </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">bo@kgs-rastede.de</li>
-                <li class="list-group-item">0049-4402-9264-22</li>
-            </ul>
-        </div>
-    </div>
-    <div class="col mb-4">
-        <div class="card h-100" style="width: 16rem;">
-            <img src="<?= url('assets/fotos_kontakte/vm.jpg') ?>" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Didaktische Leiterin</h5>
-                <p class="card-text">Hildegard Varelmann</p>
-            </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">vm@kgs-rastede.de</li>
-                <li class="list-group-item">0049-4402-9264-24</li>
-            </ul>
-        </div>
-    </div>
-    <div class="col mb-4">
-        <div class="card h-100" style="width: 16rem;">
-            <img src="<?= url('assets/fotos_kontakte/kle.jpg') ?>" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Realschulzweigleiter</h5>
-                <p class="card-text">Andreas Kleeberg</p>
-            </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">kle@kgs-rastede.de</li>
-                <li class="list-group-item">0049-4402-9264-13</li>
-            </ul>
-        </div>
-    </div>
-    <div class="col mb-4">
-        <div class="card h-100" style="width: 16rem;">
-            <img src="<?= url('assets/fotos_kontakte/id.jpg') ?>" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Leiterin Feldbreite</h5>
-                <p class="card-text">Naile Ildiz</p>
-            </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">id@kgs-rastede.de</li>
-                <li class="list-group-item">0049-4402-9264-21</li>
-            </ul>
-        </div>
-    </div>
-    <div class="col mb-4">
-        <div class="card h-100" style="width: 16rem;">
-            <img src="<?= url('assets/fotos_kontakte/dk.jpg') ?>" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Oberstufenkoordinator</h5>
-                <p class="card-text">Joachim Diekmann</p>
-            </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">dk@kgs-rastede.de</li>
-                <li class="list-group-item">0049-4402-9264-69</li>
-            </ul>
-        </div>
-    </div>
-    <div class="col mb-4">
-        <div class="card h-100" style="width: 16rem;">
-            <img src="<?= url('assets/fotos_kontakte/pb.jpg') ?>" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Hauptschulzweileiterin</h5>
-                <p class="card-text">Marieke Panneborg</p>
-            </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">pb@kgs-rastede.de</li>
-                <li class="list-group-item">0049-4402-9264-23</li>
-            </ul>
-        </div>
-    </div>
-</div>
 
+<div class="row row-cols-1 row-cols-md-4">
+<?php foreach(page('kontakte')->children() as $kontakt): ?>
+    <div class="col mb-4">
+        <div class="card h-100" style="width: 16rem;">
+            <img src="<?= $kontakt->images()->first()->url() ?>" class="card-img-top" alt="<?= $kontakt->images()->first()->alt() ?>">
+            <div class="card-body">
+                <h5 class="card-title"><?= $kontakt->position() ?></h5>
+                <p class="card-text"><?= $kontakt->title() ?></p>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item"><?= $kontakt->email() ?></li>
+                <li class="list-group-item"><?= $kontakt->phone() ?></li>
+            </ul>
+        </div>
+    </div>
+<?php endforeach ?>
+</div>
 
 
 
