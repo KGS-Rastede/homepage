8928dc686dc9e428911317d7e00b71a6ebca18a3
Carsten Niehaus <ni@kgs-rastede.de>
2020-01-26 10:58:33 +0100

    Man kann nun das hier machen: http://kirby/search?q=ag

---
+++ site/templates/search.php
@@ -0,0 +1,21 @@
+<?php snippet('header') ?>
+
+<?php snippet('page-header') ?>
+
+
+<form>
+  <input type="search" name="q" value="<?= html($query) ?>">
+  <input type="submit" value="Search">
+</form>
+
+<ul>
+  <?php foreach ($results as $result): ?>
+  <li>
+    <a href="<?= $result->url() ?>">
+      <?= $result->title() ?>
+    </a>
+  </li>
+  <?php endforeach ?>
+</ul>
+
+<?php snippet('footer') ?>
