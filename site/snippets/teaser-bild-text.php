a4db964906332436ca300786f0bf5425308cbaea
Carsten Niehaus <ni@kgs-rastede.de>
2020-01-27 21:30:57 +0100

    besser Design

--- site/snippets/teaser-bild-text.php
+++ site/snippets/teaser-bild-text.php
@@ -1,17 +1,21 @@
-<div class="col-md-7">
-    <!-- Nun der Kartentitel -->
-    <h3 class="card-title">
-        <a href="<?= $subpage->url() ?>"><?= $subpage->title() ?></a>
-    </h3>
-    <!-- Nun die ersten 250 Zeichen -->
-    <p class="card-description">
-        <?= $subpage->Text()->blocks()->excerpt(250) ?>
-        <a href="<?= $subpage->url() ?>">...weiterlesen</a>
-    </p>
-    <p class="author">
-        von
-        <a href="#pablo">
-            <b><?= $subpage->author() ?></b>
-        </a> Datum: <?= $subpage->date()->toDate("d.m.Y") ?>
-    </p>
+<div class="col-md-6">
+    <div class="card">
+        <div class="card-header">
+            <h4 class="card-title">
+                <a href="<?= $subpage->url() ?>"><?= $subpage->title() ?></a>
+            </h4>
+            <p class="category">
+                <p class="author">
+                    von
+                    <a href="#">
+                        <b><?= $subpage->author() ?></b>
+                    </a> Datum: <?= $subpage->date()->toDate("d.m.Y") ?>
+                </p>
+            </p>
+        </div>
+        <div class="card-body">
+            <?= $subpage->Text()->blocks()->excerpt(250) ?>
+            <a href="<?= $subpage->url() ?>">...weiterlesen</a>
+        </div>
+    </div>
 </div>
