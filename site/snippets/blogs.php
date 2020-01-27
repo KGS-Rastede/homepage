2c02bc4451557fbba8cc2d7beda25f498276763a
Carsten Niehaus <ni@kgs-rastede.de>
2020-01-27 21:30:47 +0100

    auch hier das snipper anwenden

--- site/snippets/blogs.php
+++ site/snippets/blogs.php
@@ -2,33 +2,14 @@
   <div class="row">
 
     <?php foreach ($blogs as $blog) : ?>
-      <div class="col-md-5">
-        <div class="card-header card-header-image">
-          <?php if ($blog->hasImages() > 0) : ?>
-            <img src="<?= $blog->images()->first()->url() ?>" class="img img-raised" alt="<?= $blog->images()->first()->alt() ?>">
-          <?php else : ?>
-            <img src="<?= $kirby->url('assets') ?>/logo-kgs.jpg" class="img img-raised" alt="Logo der KGS">
-          <?php endif ?>
-        </div>
-      </div>
-      <div class="col-md-7">
-        <h6 class="card-category text-info">HEIR FEHLT CODE</h6>
-        <h3 class="card-title">
-          <a href="<?= $blog->url() ?>"><?= $blog->title() ?></a>
-        </h3>
-        <p class="card-description">
-          <?= $blog->Text()->blocks()->excerpt(250) ?>
-          <a href="<?= $blog->url() ?>">...weiterlesen</a>
-        </p>
-        <p class="author">
-          von
-          <a href="#pablo">
-            <b><?= $blog->author() ?></b>
-          </a> Datum: <?= $blog->date()->toDate("d.m.Y") ?>
-        </p>
-      </div>
-    <?php endforeach ?>
+      <?php snippet('teaser-bild', [
+        'subpage' => $blog
+      ]) ?>
 
+      <?php snippet('teaser-bild-text', [
+        'subpage' => $blog
+      ]) ?>
+    <?php endforeach ?>
 
   </div>
 </div>
