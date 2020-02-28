26915f1a3af93bc890d6e3972ed0601e0f5cc6ed
Carsten Niehaus <ni@kgs-rastede.de>
2020-02-28 18:10:05 +0100

    Erste Version der Blogs für den SER

---
+++ site/templates/ser.php
@@ -0,0 +1,84 @@
+<?php snippet('header') ?>
+
+<?php snippet('page-header') ?>
+
+<h2><?= $page->heading() ?></h2>
+
+<p>
+  <?= $page->text()->blocks() ?>
+</p>
+
+<div class="row">
+
+  <h2>Aktuelles aus dem Schulelternrat</h2>
+  
+<?php $index = 0;
+ foreach(page('ser/nachrichten')
+->children()
+->listed()
+
+<?php if ($index  != 0) : ?>
+
+                <?php snippet('teaser-bild', [
+                  'subpage' => $subpage
+                ]) ?>
+
+                <?php snippet('teaser-bild-text', [
+                  'subpage' => $subpage
+                ]) ?>
+
+                
+
+
+
+              <?php else : ?>
+              <?php endif ?>
+            <?php endforeach?>
+
+
+</div>
+
+
+<div class="container">
+  <div class="row">
+    
+      
+
+        <?php snippet('blogs', [
+          'blogs' => page('blogs')
+            ->children()
+            ->listed()
+            ->filterBy('tags', $page->haupttag(), ',')
+        ]) ?>
+
+
+        <?php if ($page->fotoansicht() == 'carousel') : ?>
+          <?php snippet('carousel') ?>
+        <?php elseif ($page->fotoansicht() == 'gallery') : ?>
+          <?php snippet('gallery') ?>
+        <?php else : ?>
+          <!-- Bilder werden vom Autor manuel gesetzt -->
+        <?php endif ?>  
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php snippet('footer') ?>
