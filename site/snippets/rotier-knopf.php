fbb4d25559064b195bbf8b5f1ab2ef510765ea5b
Fynn Kunzmann <max@mustermann.de>
2020-03-01 18:24:31 +0100

    Download funktoniert noch nicht

--- site/snippets/rotier-knopf.php
+++ site/snippets/rotier-knopf.php
@@ -3,8 +3,8 @@
         <div class="card-body">
             <h3 class="card-title"><?= $subpage->titel() ?></h3>
             <p class="card-description"><?= $subpage->text() ?></p>
-            <?php if($image = $subpage->datei()->toFile()): ?>
-                <a href="<?php $subpage->datei()->toFile() ?>" download="18.png" class="btn btn-warning btn-round">Download</a>
+            <?php if($file = $subpage->datei()->toFile()): ?>
+                <a href="<?php $file->url() ?>" download="<?php $file->filename() ?>" class="btn btn-warning btn-round">Download</a>
             <?php endif ?>
             <?php if($subpage->link() != ""): ?>
                 <a href="<?= $subpage->link() ?>" class="btn btn-warning btn-round" role="button">Mehr zum Thema</a>
