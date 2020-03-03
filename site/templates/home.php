9d85e00171c78a6b74e60576738e343827c13b40
Carsten Niehaus <ni@kgs-rastede.de>
2020-03-03 15:53:45 +0100

    #33 Presse auf der Startseite

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,110 +1,148 @@
 <?php snippet('header') ?>
 <?php snippet('page-header') ?>
 
-<!-- 
-  An besonderen Tagen (Schneefrei, Heizungsburch, ...) soll ganz schnell
-  ein Banner angezeigt werden, damit die Eltern schnell informiert werden können
- -->
 
-<?php if(page('allgemeines/notfall')->category() == "sturm"):
-        $titel = "Schulausfall wegen Sturm";
-endif ?>
-<?php if(page('allgemeines/notfall')->category() == "eis"):
-        $titel = "Schulausfall wegen Glatteis";
-endif ?>
-<?php if(page('allgemeines/notfall')->category() == "regen"):
-        $titel = "Schulausfall wegen Starkregen";
-endif ?>
-<?php if(page('allgemeines/notfall')->category() == "heizung"):
-        $titel = "Schulausfall wegen Heizungsausfall";
-endif ?>
-<?php if(page('allgemeines/notfall')->category() == "eigenerTitel"):
-        $titel = page('allgemeines/notfall')->textTitel();
-endif ?>
-
-<?php if(page('allgemeines/notfall')->toggle()->bool() === true) :?>
-  <div class="alert alert-danger" role="alert">
-    <h2><?php echo $titel ?></h2>
-    <p class="lead"><?= page('allgemeines/notfall')->text() ?></p>
-  </div>
-<?php endif ?>
-        
-
-<!-- Rotierknopf bspw für Elternsprechtag -->
-
-<div class="row">
-  <?php foreach (page('allgemeines/aktuelles')->banner()->toStructure() as $subpage) :  ?>
-          <?php if ($subpage->dateBis()->toDate('Y-m-d') >= date('Y-m-d')) :
-          snippet('rotier-knopf', ['subpage' => $subpage]);
-          endif ?>
-  <?php endforeach ?>
-</row>
-<!-- -->
-
-<div class="blogs-1" id="blogs-1">
-  <div class="container">
+<div class="container">
+  <div class="content">
     <div class="row">
-      <div class="col-md-10 ml-auto mr-auto">
-        <h2 class="title">Aktuelle Nachrichten</h2>
-        <br>
+      <div class="col-md-9">
+        <!-- 
+        An besonderen Tagen (Schneefrei, Heizungsburch, ...) soll ganz schnell
+        ein Banner angezeigt werden, damit die Eltern schnell informiert werden können
+        -->
 
-        <!--  
-              Jetzt werden die Elemente angefügt. 
-              -->
+        <?php if (page('allgemeines/notfall')->category() == "sturm") :
+          $titel = "Schulausfall wegen Sturm";
+        endif ?>
+        <?php if (page('allgemeines/notfall')->category() == "eis") :
+          $titel = "Schulausfall wegen Glatteis";
+        endif ?>
+        <?php if (page('allgemeines/notfall')->category() == "regen") :
+          $titel = "Schulausfall wegen Starkregen";
+        endif ?>
+        <?php if (page('allgemeines/notfall')->category() == "heizung") :
+          $titel = "Schulausfall wegen Heizungsausfall";
+        endif ?>
+        <?php if (page('allgemeines/notfall')->category() == "eigenerTitel") :
+          $titel = page('allgemeines/notfall')->textTitel();
+        endif ?>
 
-        <div class="card card-plain card-blog">
-          <div class="row">
-
-            <!-- der ersten beiden Zeilen sind immer gleich -->
-
-
-
-            <?php $index = 0;
-            foreach (page('blogs')->children()->filterBy('featured', true) as $subpage) : $index++ ?>
-
-              <!--  
-                % 2 testet letztlich, ob der Wert in $index gerade ist oder nicht
-                Das heißt: "glatt durch 2 teilbar".
-
-                Ich möchte effektiv bei jedem zweiten Artikel das Bild links und 
-                den Text rechts haben, um das Design etwas aufzulockern
-              -->
-              <?php if ($index % 2 == 0) : ?>
-
-                <?php snippet('teaser-bild', [
-                  'subpage' => $subpage
-                ]) ?>
-
-                <?php snippet('teaser-bild-text', [
-                  'subpage' => $subpage
-                ]) ?>
-
-              <?php else : ?>
-
-                <?php snippet('teaser-bild-text', [
-                  'subpage' => $subpage
-                ]) ?>
-
-                <?php snippet('teaser-bild', [
-                  'subpage' => $subpage
-                ]) ?>
-
-              <?php endif ?>
+        <?php if (page('allgemeines/notfall')->toggle()->bool() === true) : ?>
+          <div class="alert alert-danger" role="alert">
+            <h2><?php echo $titel ?></h2>
+            <p class="lead"><?= page('allgemeines/notfall')->text() ?></p>
+          </div>
+        <?php endif ?>
 
 
 
-            <?php endforeach ?>
 
-            <!-- der letzten beiden Zeilen sind immer gleich -->
+        <!-- Rotierknopf bspw für Elternsprechtag -->
+
+        <div class="row">
+          <?php foreach (page('allgemeines/aktuelles')->banner()->toStructure() as $subpage) :  ?>
+            <?php if ($subpage->dateBis()->toDate('Y-m-d') >= date('Y-m-d')) :
+              snippet('rotier-knopf', ['subpage' => $subpage]);
+            endif ?>
+          <?php endforeach ?>
+        </div>
+
+
+
+
+        <div class="blogs-1" id="blogs-1">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-9 ml-auto mr-auto">
+                <h2 class="title">Aktuelle Nachrichten</h2>
+                <br>
+
+                <!--  
+                    Jetzt werden die Elemente angefügt. 
+                    -->
+
+                <div class="card card-plain card-blog">
+                  <div class="row">
+
+                    <!-- der ersten beiden Zeilen sind immer gleich -->
+
+
+
+                    <?php $index = 0;
+                    foreach (page('blogs')->children()->filterBy('featured', true) as $subpage) : $index++ ?>
+
+                      <!--  
+                      % 2 testet letztlich, ob der Wert in $index gerade ist oder nicht
+                      Das heißt: "glatt durch 2 teilbar".
+
+                      Ich möchte effektiv bei jedem zweiten Artikel das Bild links und 
+                      den Text rechts haben, um das Design etwas aufzulockern
+                    -->
+                      <?php if ($index % 2 == 0) : ?>
+
+                        <?php snippet('teaser-bild', [
+                          'subpage' => $subpage
+                        ]) ?>
+
+                        <?php snippet('teaser-bild-text', [
+                          'subpage' => $subpage
+                        ]) ?>
+
+                      <?php else : ?>
+
+                        <?php snippet('teaser-bild-text', [
+                          'subpage' => $subpage
+                        ]) ?>
+
+                        <?php snippet('teaser-bild', [
+                          'subpage' => $subpage
+                        ]) ?>
+
+                      <?php endif ?>
+
+
+
+                    <?php endforeach ?>
+
+                    <!-- der letzten beiden Zeilen sind immer gleich -->
+                  </div>
+                </div>
+
+                <!--  -->
+
+              </div>
+            </div>
           </div>
         </div>
 
-        <!--  -->
 
+
+
+      </div>
+      <div class="col-md-3">
+        <div class="card">
+          <div class="card-header card-header-info">
+            <h5 class="card-title">Aus der Presse</h5>
+          </div>
+          <div class="card-body">
+            <div class="list-group">
+                <a href="#" class="list-group-item list-group-item-action">Presseartikel 1</a></li>
+                <a href="#" class="list-group-item list-group-item-action">Presseartikel 1</a></li>
+                <a href="#" class="list-group-item list-group-item-action">Presseartikel 1</a></li>
+                <a href="#" class="list-group-item list-group-item-action">Presseartikel 1</a></li>
+                <a href="#" class="list-group-item list-group-item-action">Presseartikel 1</a></li>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </div>
+
+
+
+
+
 
 
 <?php snippet('footer') ?>
