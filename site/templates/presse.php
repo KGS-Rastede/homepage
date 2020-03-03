98068ba3e270043ed69c0c4e7461e361bbc9394f
Carsten Niehaus <ni@kgs-rastede.de>
2020-03-03 14:47:50 +0100

    Pagination funktioneirt im Ansatz

--- site/templates/presse.php
+++ site/templates/presse.php
@@ -3,6 +3,76 @@
 <?php snippet('page-header') ?>
 
 
+
+
+<!--  -->
+
+<?php 
+// `toStructure()` erzeugt einen Iterator fuer die URLs
+$items = $page->pressenachrichten()->toStructure();
+
+$list = $items->paginate(2);
+
+// Nun kann man jede URL durchgehen und jeweils einen Links erzeugen
+foreach ($list as $item): ?>
+ 
+  <h2 class="title">
+    <a href="<?= $item->link() ?>">
+      <?= $item->name()->html() ?>
+      
+    </a>
+  </h2>
+  <div class="text-right">
+  Datum: <?= $item->datum()->html() ?> <span class="badge badge-pill alert-dark"><?= $item->zeitung()->html() ?></span>
+  </div>
+
+  <p><?= $item->anfang() ?></p>
+<?php endforeach ?>
+
+
+
+<!--  -->
+
+
+<?php $pagination = $list->pagination() ?>
+<nav>
+  <ul class="pagination">
+
+    <?php if ($pagination->hasPrevPage()): ?>
+    <li class="page-item">
+      <a href="<?= $pagination->prevPageURL() ?>">‹</a>
+    </li>
+    <?php else: ?>
+    <li>
+      <span>‹</span>
+    </li>
+    <?php endif ?>
+
+    <!-- Hier nun die mittleren Elemente -->
+
+    <?php foreach ($pagination->range(10) as $r): ?>
+    <li class="page-item">
+      <a class="page-link" <?= $pagination->page() === $r ? ' aria-current="page"' : '' ?> href="<?= $pagination->pageURL($r) ?>">
+        <?= $r ?>
+      </a>
+    </li>
+    <?php endforeach ?>
+
+    <?php if ($pagination->hasNextPage()): ?>
+    <li class="page-item">
+      <a href="<?= $pagination->nextPageURL() ?>">›</a>
+    </li>
+    <?php else: ?>
+    <li class="page-item">
+      <span>›</span>
+    </li>
+    <?php endif ?>
+
+  </ul>
+</nav>
+
+<!-- hier nun die Anleitung aus Bootstrap -->
+
 <nav aria-label="presse-navigation">
   <ul class="pagination">
 
@@ -24,28 +94,6 @@
 
   </ul>
 </nav>
-
-<!--  -->
-
-<?php 
-// `toStructure()` erzeugt einen Iterator fuer die URLs
-$items = $page->pressenachrichten()->toStructure();
-
-// Nun kann man jede URL durchgehen und jeweils einen Links erzeugen
-foreach ($items->paginate(2) as $item): ?>
- 
-  <h2 class="title">
-    <a href="<?= $item->link() ?>">
-      <?= $item->name()->html() ?>
-      
-    </a>
-  </h2>
-  <div class="text-right">
-  Datum: <?= $item->datum()->html() ?> <span class="badge badge-pill alert-dark"><?= $item->zeitung()->html() ?></span>
-  </div>
-
-  <p><?= $item->anfang() ?></p>
-<?php endforeach ?>
 
 
 <?php snippet('footer') ?>
