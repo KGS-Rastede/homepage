f1058b24201126cd60f2f1aaf1c405aabddcdfec
Carsten Niehaus <ni@kgs-rastede.de>
2020-03-19 13:03:40 +0100

    unnötiges Zeug weg

--- site/templates/kalender.php
+++ site/templates/kalender.php
@@ -33,24 +33,11 @@ document.addEventListener('DOMContentLoaded', function() {
       list:     'Liste'
     },
 
-    displayEventTime: false, // don't show the time column in list view
+    displayEventTime: true, // don't show the time column in list view
 
-    // THIS KEY WON'T WORK IN PRODUCTION!!!
-    // To make your own Google API key, follow the directions here:
-    // http://fullcalendar.io/docs/google_calendar/
     googleCalendarApiKey: 'AIzaSyCYHdh89aVp9EA9nh4e_kMDy6ugfVuRHoE',
 
-    // US Holidays
-    events: 'kooperativegesamtschulerastede@gmail.com',
-
-    eventClick: function(arg) {
-
-      // opens events in a popup window
-      window.open(arg.event.url, '_blank', 'width=700,height=600');
-
-      // prevents current tab from navigating
-      arg.jsEvent.preventDefault();
-    }
+    events: 'kooperativegesamtschulerastede@gmail.com'
 
   });
 
