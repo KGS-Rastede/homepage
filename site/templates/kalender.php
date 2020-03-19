77417cd992ee679ed61dd9e826c69e94225c64b3
Carsten Niehaus <ni@kgs-rastede.de>
2020-03-19 10:58:01 +0100

    Zwischenstand festhalten, so geht es...

--- site/templates/kalender.php
+++ site/templates/kalender.php
@@ -10,12 +10,11 @@
 </style>
 
 <script>
-
   document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
 
     var calendar = new FullCalendar.Calendar(calendarEl, {
-      plugins: [ 'interaction', 'dayGrid', 'timeGrid', 'list' ],
+      plugins: ['interaction', 'dayGrid', 'timeGrid', 'list'],
       header: {
         left: 'prev,next today',
         center: 'title',
@@ -44,13 +43,68 @@
       //   }
       // ]
 
+      //   events: [{
+      //       title: 'All Day Event',
+      //       start: '2020-02-01'
+      //     },
+      //     {
+      //       title: 'Long Event',
+      //       start: '2020-02-07',
+      //       end: '2020-02-10'
+      //     },
+      //     {
+      //       groupId: 999,
+      //       title: 'Repeating Event',
+      //       start: '2020-02-09T16:00:00'
+      //     },
+      //     {
+      //       groupId: 999,
+      //       title: 'Repeating Event',
+      //       start: '2020-02-16T16:00:00'
+      //     },
+      //   ]
+      // });
 
-      events: '<?= $kirby->url('assets') ?>/kgs.json'
+
+      events: [{
+          title: 'All Day Event',
+          start: '2020-02-01'
+        },
+        {
+          title: 'Büro einräumen',
+          start: '2019-02-01 15: 00: 00+01: 00',
+          end: '2019-02-01 17: 00: 00+01: 00'
+        },
+        {
+          title: '2 Tag, 12 und 13. März ',
+          start: '2020-03-12',
+          end: '2020-03-14'
+        },
+
+        {
+        title: '2 Tag, 12 und 13. März',
+        start: '2020-03-12',
+        end: '2020-03-14'
+        },
+      
+
+        {
+          title: 'Testevent 1 ganztägig 10. März',
+          start: '2020-03-10',
+          end: '2020-03-11'
+        },
+        {
+          groupId: 999,
+          title: 'Repeating Event',
+          start: '2020-02-16T16:00:00'
+        },
+      ]
     });
+
+
 
     calendar.render();
   });
-
 </script>
 
 <div id='calendar'></div>
