3e7a544397f66f6d11f2fdf1e1543de1152b60a3
Carsten Niehaus <ni@kgs-rastede.de>
2020-03-21 14:30:19 +0100

    nicht schön, aber wenigstens nicht mehr kaputt

--- site/templates/blogs.php
+++ site/templates/blogs.php
@@ -5,90 +5,18 @@
 
 <section class="jumbotron text-center">
   <div class="container">
-    <h1>Die Fächer stellen sich vor</h1>
+    <h1>Die Schule stellt sich vor</h1>
 
-    <p class="lead">Hier stellen sich die Fächer und Fachbereiche vor. Klicken Sie auf das Symbolbild, um aktuelle Nachrichten zu lesen!</p>
+    <p class="lead">Hier stellen sich die Fächer und Fachbereiche vor, aber auch andere Bereiche der Schule berichten.</p>
   </div>
 </section>
 
 <section class="content blog">
-
-  <h1><?= $page->title()->html() ?></h1>
-  <?= $page->text()->kirbytext() ?>
-
-  <?php $index = 0; foreach ($page->children()->listed()->flip() as $subpage) : $index++ ?>
-
-
-    <?php if ($index % 2 == 0) : ?>
-
-      <div class="col-md-5">
-        <div class="card-header card-header-image">
-          <?php if ($subpage->hasImages() > 0) : ?>
-            <img src="<?= $subpage->images()->first()->url() ?>" class="img img-raised" alt="<?= $subpage->images()->first()->alt() ?>">
-          <?php else : ?>
-            <img src="<?= $kirby->url('assets') ?>/logo-kgs.jpg" class="img img-raised" alt="Logo der KGS">
-          <?php endif ?>
-        </div>
-      </div>
-      <div class="col-md-7">
-        <h6 class="card-category text-info">Artikel #<?= $index ?></h6>
-        <h3 class="card-title">
-          <a href="<?= $subpage->url() ?>"><?= $subpage->title() ?></a>
-        </h3>
-        <p class="card-description">
-          <?= $subpage->Text()->blocks()->excerpt(250) ?>
-          <a href="<?= $subpage->url() ?>">...weiterlesen</a>
-        </p>
-        <p class="author">
-          by
-          <a href="#pablo">
-            <b><?= $subpage->author() ?></b>
-          </a> Datum: <?= $subpage->date()->toDate("d.m.Y") ?>
-        </p>
-      </div>
-
-    <?php else : ?>
-
-
-
-      <div class="card card-plain card-blog">
-        <div class="row">
-          <div class="col-md-7">
-            <h3 class="card-title">
-              <a href="#pablo"><?= $subpage->title() ?></a>
-            </h3>
-            <p class="card-description">
-              <?= $subpage->Text()->blocks()->excerpt(250) ?>
-              <a href="<?= $subpage->url() ?>">...weiterlesen</a>
-            </p>
-            <p class="author">
-              by
-              <a href="#pablo">
-                <b><?= $subpage->author() ?></b>
-              </a> Datum: <?= $subpage->date()->toDate("d.m.Y") ?>
-            </p>
-          </div>
-          <div class="col-md-5">
-            <div class="card-header card-header-image">
-
-              <?php if ($subpage->hasImages() > 0) : ?>
-                <img src="<?= $subpage->images()->first()->url() ?>" class="img img-raised" alt="<?= $subpage->images()->first()->alt() ?>">
-              <?php else : ?>
-                <img src="<?= $kirby->url('assets') ?>/logo-kgs.jpg" class="img img-raised" alt="Logo der KGS">
-              <?php endif ?>
-
-
-            </div>
-          </div>
-        </div>
-      </div>
-
-
-    <?php endif ?>
-
-
-
-  <?php endforeach ?>
+<?php snippet('blogs', [
+      'blogs' => page('blogs')
+        ->children()
+        ->listed()
+    ]) ?>
 
 </section>
 
