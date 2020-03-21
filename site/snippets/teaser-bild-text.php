855534837454e357b7ee3b1dd14a98ba408e78d6
Carsten Niehaus <ni@kgs-rastede.de>
2020-03-21 18:17:07 +0100

    Auch hier den Blog schicker machen

--- site/snippets/teaser-bild-text.php
+++ site/snippets/teaser-bild-text.php
@@ -1,21 +1,19 @@
 <div class="col-md-6 mt-4">
-    <div class="card">
-        <div class="card-header">
-            <h4 class="card-title">
-                <a href="<?= $subpage->url() ?>"><?= $subpage->title() ?></a>
-            </h4>
-            <p class="category">
-                <p class="author">
-                    von
-                    <a href="#">
-                        <b><?= $subpage->author() ?></b>
-                    </a> Datum: <?= $subpage->date()->toDate("d.m.Y") ?>
-                </p>
-            </p>
-        </div>
-        <div class="card-body">
+    <div class="card-body">
+        <h4 class="card-title">
+            <a href="<?= $subpage->url() ?>"><?= $subpage->title() ?></a>
+        </h4>
+        <p class="card-category">
+            <div class="author">
+                von
+                <a href="#">
+                    <b><?= $subpage->author() ?></b>
+                </a> Datum: <?= $subpage->date()->toDate("d.m.Y") ?>
+            </div>
+        </p>
+        <div class="card-description">
             <?= $subpage->Text()->blocks()->excerpt(250) ?>
-            <a href="<?= $subpage->url() ?>">...weiterlesen</a>
+            <a href="<?=     $subpage->url() ?>">...weiterlesen</a>
         </div>
     </div>
 </div>
