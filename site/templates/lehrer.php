854476df25ff7293c9795c50df369df910397987
Carsten Niehaus <ni@kgs-rastede.de>
2020-03-22 14:54:38 +0100

    Tabelle wird nun auch generiert

---
+++ site/templates/lehrer.php
@@ -0,0 +1,49 @@
+<?php snippet('header') ?>
+<?php snippet('page-header') ?>
+
+
+<h2><?= $page->heading() ?></h2>
+
+
+<div class="col-lg-8 col-md-10 ml-auto mr-auto">
+  <div class="table-responsive">
+    <table class="table">
+    <thead>
+          
+            <th>Lehrplanname</th>
+            <th>Kürzel</th>
+            <th>Funktion</th>
+            <th>Fächer</th>
+
+          
+        </thead>
+      <tbody>
+        <?php foreach ($page->children() as $l) : ?>
+
+
+          <tr>
+            <td>
+              <?= $l->name() ?>
+            </td>
+            <td>
+              <?= $l->kuerzel() ?>
+            </td>
+            <td>
+              <?= $l->funktion() ?>
+            </td>
+            <td>
+              <?= $l->faecher() ?>
+            </td>
+          </tr>
+
+        <?php endforeach ?>
+
+
+      </tbody>
+    </table>
+  </div>
+</div>
+
+
+
+<?php snippet('footer') ?>
