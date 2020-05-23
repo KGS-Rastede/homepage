4b061c712f3758e663702e33922e5db5ca154084
Carsten Niehaus <ni@kgs-rastede.de>
2020-05-23 12:08:06 +0200

    lokales css/js

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,17 +1,3 @@
-<!--
- =========================================================
- Material Kit PRO - v2.2.0
- =========================================================
-
- Product Page: https://themes.getbootstrap.com/product/material-kit-pro/
- Copyright 2019 Creative Tim (https://www.creative-tim.com)
-
- Coded by Creative Tim
-
- =========================================================
-
- The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
--->
 <!doctype html>
 <html lang="en">
 <head>
@@ -21,8 +7,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-
+  <link rel="stylesheet" href="<?= $kirby->url('assets') ?>/css/bootstrap.min.css" >
 
   <link rel="stylesheet" href="<?= $kirby->url('assets') ?>/css/kgs.css" >
 
