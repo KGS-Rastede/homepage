6dc602e3fe6b25148e1c61dc72883f7014809e00
Carsten Niehaus <ni@kgs-rastede.de>
2020-05-23 15:42:22 +0200

    alles etwas dezenter

--- site/snippets/rotier-knopf.php
+++ site/snippets/rotier-knopf.php
@@ -1,13 +1,13 @@
 <div class="col-md-6 ml-auto mr-auto">
-    <div class="card bg-success text-center">
-        <div class="card-body">
+    <div class="card border-dark mb-3" style="max-width: 18rem;">
+        <div class="card-body text-dark">
             <h3 class="card-title"><?= $subpage->titel() ?></h3>
-            <p class="card-description"><?= $subpage->text() ?></p>
+            <p class="card-text"><?= $subpage->text() ?></p>
             <?php if($file = $subpage->datei()->toFile()): ?>
-                <a href="<?php echo $file->url() ?>" download="<?php echo $file->filename() ?>" class="btn btn-warning btn-round"> <img src="<?= $kirby->url('assets') ?>/icons/info-square.svg"><?php echo $file->name() ?></a>
+                <a href="<?php echo $file->url() ?>" download="<?php echo $file->filename() ?>" class="btn btn-primary btn-round"> <img src="<?= $kirby->url('assets') ?>/icons/info-square.svg"><?php echo $file->name() ?></a>
             <?php endif ?>
             <?php if($subpage->link() != ""): ?>
-                <a href="<?= $subpage->link() ?>" class="btn btn-warning btn-round" role="button"> <img src="<?= $kirby->url('assets') ?>/icons/info-square.svg">Mehr zum Thema</a>
+                <a href="<?= $subpage->link() ?>" class="btn btn-secondary" role="button"> <img src="<?= $kirby->url('assets') ?>/icons/info-square.svg">Mehr zum Thema</a>
             <?php endif ?>
         </div>
     </div>
