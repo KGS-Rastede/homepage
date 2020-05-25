6aab2a41c146a103b602431d0c206e83b887f2ad
Carsten Niehaus <ni@kgs-rastede.de>
2020-05-25 16:49:20 +0200

    Anpassungen der Farben

--- site/templates/home.php
+++ site/templates/home.php
@@ -48,6 +48,34 @@
         <?php endforeach ?>
       </div>
 
+      <div class="card py-4">
+
+        <h1>Zum Testen der Farben hier die Farben des Schemas</h1>  
+
+        <hr>
+
+        <h1>primary</h1>
+        <button class="btn btn-primary">Weitere Nachrichten aus der Schule</button>
+
+        <h1>secondary</h1>
+        <button class="btn btn-secondary">Weitere Nachrichten aus der Schule</button>
+
+        <h1>info</h1>
+        <button class="btn btn-info">Weitere Nachrichten aus der Schule</button>
+
+        <h1>warning</h1>
+        <button class="btn btn-warning">Weitere Nachrichten aus der Schule</button>
+
+        <h1>danger</h1>
+        <button class="btn btn-danger">Weitere Nachrichten aus der Schule</button>
+
+        <h1>light</h1>
+        <button class="btn btn-light">Weitere Nachrichten aus der Schule</button>
+
+        <h1>dark</h1>
+        <button class="btn btn-dark">Weitere Nachrichten aus der Schule</button>
+      </div>
+
 
 
 
