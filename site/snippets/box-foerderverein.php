bc42c4296203c502b32f7190d12dda8f99c7d847
Carsten Niehaus <ni@kgs-rastede.de>
2020-06-01 21:24:48 +0200

    Schriftfarbe

--- site/snippets/box-foerderverein.php
+++ site/snippets/box-foerderverein.php
@@ -1,6 +1,6 @@
 <div class="card mb-1">
-    <div class="card-body ">
-        <h4 class="card-category text-primary">Der Förderverein</h4>
+    <div class="card-body">
+        <h4 class="card-category">Der Förderverein</h4>
         <ul>
             Hier fehlt Text
         </ul>
