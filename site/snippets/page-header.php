4330008b9d92b25383b0d57156e8a430012f650b
Carsten Niehaus <ni@kgs-rastede.de>
2020-07-11 15:57:00 +0200

    Jumbotron gibt es nicht mehr, aber so ist es fast behoben

--- site/snippets/page-header.php
+++ site/snippets/page-header.php
@@ -1,20 +1,47 @@
 <main role="main">
 
-<?php if ($image = $page->hintergrundbild()->toFile()) : ?>
-  <div class="jumbotron jumbotron-fluid" style="background-image: url('<?= $image->url() ?>'); background-size: cover;">
-<?php else : ?>
-  <div class="jumbotron jumbotron-fluid" style="background-image: url('<?= $kirby->url('assets') ?>/img/banner_eng.jpg'); background-size: cover;">
-<?php endif ?>
 
-    <div class="titelfeld">
-      <div class="container">
-        <h1><?= $page->title() ?>
-          <small class="text-muted"><?= $page->subtitle() ?></small>
-        </h1>
+  <?php
+
+  $bildpfad = "";
+
+  if ($image = $page->hintergrundbild()->toFile()) : ?>
+    $bildpfad = <?= $image->url() ?>;
+  <?php else : ?>
+    $bildpfad = <?= $kirby->url('assets') ?> + "/img/banner_eng.jpg";
+  <?php endif ?>
+
+
+
+
+  <div class="p-5 text-center bg-image" style="
+      background-image: url('https://mdbootstrap.com/img/new/slides/041.jpg');
+      height: 400px;
+    ">
+    <div class="mask" style="background-color: rgba(0, 0, 0, 0.6);">
+      <div class="d-flex justify-content-center align-items-center h-100">
+        <div class="text-white">
+          <h1 class="mb-3"><?= $page->title() ?></h1>
+          <h4 class="mb-3"><?= $page->subtitle() ?></h4>
+          <a class="btn btn-outline-light btn-lg" href="#!" role="button">Call to action</a>
+        </div>
       </div>
     </div>
   </div>
 
 
-<!-- Ab hier der richtige Inhalt, der auf jeder Seite individuell sein kann -->
-<div class="container">
+
+
+
+  <div class="titelfeld">
+    <div class="container">
+      <h1><?= $page->title() ?>
+        <small class="text-muted"><?= $page->subtitle() ?></small>
+      </h1>
+    </div>
+  </div>
+  </div>
+
+
+  <!-- Ab hier der richtige Inhalt, der auf jeder Seite individuell sein kann -->
+  <div class="container">
