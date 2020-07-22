54e7e1029fc46de4ff08ccca17d8511b357b8d63
Carsten Niehaus <ni@kgs-rastede.de>
2020-07-22 22:02:47 +0200

    gleiches Design wie im Hauptblog.... Muss in einem Snippet vereinheitlicht werden.

--- site/templates/ser.php
+++ site/templates/ser.php
@@ -5,23 +5,45 @@
 <?php snippet('sidebar') ?>
 
 
-<div class="row">
-
-  <h2>Aktuelles aus dem Schulelternrat</h2>
-
-  <div class="container">
-    <div class="row">
 
 
+<?php
+// `toStructure()` erzeugt einen Iterator fuer die URLs
+$items = page('ser/nachrichten')->children()->listed();
 
-      <?php snippet('blogs', [
-        'blogs' => page('ser/nachrichten')
-          ->children()
-          ->listed()
-      ]) ?>
+$list = $items->paginate(5);
 
+// Nun kann man jede URL durchgehen und jeweils einen Links erzeugen
+foreach ($list as $item) : ?>
+
+  <div class="col-md-12 mt-4">
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title">
+          <a href="<?= $item->url() ?>"><?= $item->title() ?></a>
+        </h4>
+        <p class="card-category">
+          <div class="author text-right">
+            von
+            <b><?= $item->author() ?></b>
+            Datum: <?= $item->date()->toDate("d.m.Y") ?>
+          </div>
+        </p>
+        <p class="card-text">
+          <?= $item->Text()->blocks()->excerpt(250) ?>
+        </p>
+        <a href="<?= $item->url() ?>" class="btn btn-secondary">weiterlesen &#8594;
+        </a>
+      </div>
     </div>
+
   </div>
-</div>
+  <hr>
+<?php endforeach ?>
+
+
+
+
+
 
 <?php snippet('footer') ?>
