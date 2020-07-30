73630760022fceeec1b0ec60e4b53ac364793b19
Carsten Niehaus <ni@kgs-rastede.de>
2020-07-30 21:16:24 +0200

    #41 das Snippet nutzen, so ist der Code schön

--- site/snippets/blog-schlicht.php
+++ site/snippets/blog-schlicht.php
@@ -14,14 +14,9 @@
               Datum: <?= $item->date()->toDate("d.m.Y") ?>
             </div>
 
-            <div class="author text-right">
-              <?php foreach ($item->tags()->split() as $tag) : ?>
-               <a href="<?= url('blogs', ['params' => ['tag' => $tag]]) ?>">
-                  <span class="badge rounded-pill bg-info"><?= $tag ?></span>
-                </a>
-              <?php endforeach ?>
-
-            </div>
+            <?php snippet('tagliste', [
+              'item' => $item
+            ]) ?>
           </p>
           <p class="card-text">
             <?= $item->Text()->blocks()->excerpt(250) ?>
