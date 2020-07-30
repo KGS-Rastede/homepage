3485657eb7451b0c5a3c307ecd5d375f8eca7fec
Carsten Niehaus <ni@kgs-rastede.de>
2020-07-30 21:17:09 +0200

    #41 auch hier die Tags anzeigen

--- site/templates/blog.php
+++ site/templates/blog.php
@@ -10,16 +10,20 @@
     Autor: <?= $page->author() ?>
 
   </div>
+
+  <?php snippet('tagliste', [
+    'item' => $page
+  ]) ?>
 </div>
 
 <?= $page->text()->blocks() ?>
 
 <?php if ($page->fotoansicht() == 'carousel') : ?>
   <?php snippet('carousel') ?>
-<?php elseif($page->fotoansicht() == 'gallery') : ?> 
+<?php elseif ($page->fotoansicht() == 'gallery') : ?>
   <?php snippet('gallery') ?>
-<?php else: ?> 
-<!-- Bilder werden vom Autor manuel gesetzt -->
+<?php else : ?>
+  <!-- Bilder werden vom Autor manuel gesetzt -->
 <?php endif ?>
 
 
