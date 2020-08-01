672f6cede4ae37e9550f23326ee47166a8709496
Carsten Niehaus <ni@kgs-rastede.de>
2020-08-01 16:03:47 +0200

    css nutzen, Design

--- site/templates/devblogarticle.php
+++ site/templates/devblogarticle.php
@@ -2,14 +2,37 @@
 
 <?php snippet('page-header') ?>
 
-<section class="content article">
-  <article>
-    <?= $page->intro()->kirbytext() ?>
-    <?= $page->text()->blocks() ?>
+<div class="row">
+  <div class="col-md-12">
+    <div class="blogpost">
+      <div class="container">
+        <?= $page->intro()->kirbytext() ?>
 
-    <a href="<?= url('devblog') ?>">Zurück zur Übersicht...</a>
+        <div class="text-right">
+          <svg class="bi" width="24" height="24">
+            <use xlink:href="<?= $kirby->url('assets') ?>/icons/bootstrap-icons.svg#tags" />
+          </svg> Tags:
 
-  </article>
-</section>
+          <?php foreach ($page->tags()->split() as $tag) : ?>
+            <a href="<?= url('devblog', ['params' => ['tag' => $tag]]) ?>">
+              <span class="badge rounded-pill bg-info"><?= $tag ?></span>
+            </a>
+          <?php endforeach ?>
+        </div>
+        <?= $page->text()->blocks() ?>
+
+        <div class="text-right">
+          <a href="<?= url('devblog') ?>" class="btn btn-secondary">
+            zurück zur Übersicht &#8594;
+          </a>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
 
 <?php snippet('footer') ?>
