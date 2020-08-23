aa62d84b5a973906f01e942908ffd4b65fb39189
Carsten Niehaus <ni@kgs-rastede.de>
2020-08-23 11:29:09 +0200

    Karte nehmen, das macht den Code viel kürzer

--- site/templates/home.php
+++ site/templates/home.php
@@ -110,43 +110,9 @@
 
 
             <?php if ($subpage->datumStartseite()->toDate('Y-m-d-H-i-s') >= date('Y-m-d-H-i-s')) : ?>
-              <div class="col">
-                <div class="card">
-                  <div class="card-body">
-                    <h2 class="card-title">
-                      <?= $subpage->title() ?>
-                    </h2>
-
-                    <div class="text-right">
-                      <svg class="bi" width="24" height="24">
-                        <use xlink:href="<?= $kirby->url('assets') ?>/icons/bootstrap-icons.svg#tags" />
-                      </svg>
-                      <?php foreach ($subpage->tags()->split() as $tag) : ?>
-                        <a href="<?= url('blogs', ['params' => ['tag' => $tag]]) ?>">
-                          <span class="badge rounded-pill bg-warning"><?= $tag ?></span>
-                        </a>
-                      <?php endforeach ?>
-
-                      - <b><?= $subpage->author() ?></b> - <?= $subpage->date()->toDate("d.m.Y") ?>
-                    </div>
-
-                    <p class="card-text mt-2">
-                      <?= $subpage->Text()->blocks()->excerpt(250) ?>
-                    </p>
-                  </div>
-
-
-                  <div class="card-footer">
-                    <div class="text-right">
-                      <a href="<?= $subpage->url() ?>">
-                        <button type="button" class="btn btn-warning">...weiterlesen</button>
-                      </a>
-                    </div>
-
-
-                  </div>
-                </div>
-              </div>
+              <?php
+                snippet('blogkarte', ['subpage' => $subpage]);
+              ?>
             <?php endif ?>
 
 
