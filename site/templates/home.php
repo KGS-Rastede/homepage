2fb83d0207f3468a38dff40abf510840add8e991
Carsten Niehaus <ni@kgs-rastede.de>
2020-10-05 09:18:36 +0200

    auf meinen UHD-Bildschirmen muss ich tatsächlich xxl einführen, sonst ist das viiiiiiiel zu breit..

--- site/templates/home.php
+++ site/templates/home.php
@@ -4,7 +4,7 @@
 
 <div class="container-fluid">
   <div class="row">
-    <div class="col-xl-8">
+    <div class="col-xl-8 col-xxl-10">
 
 
       <div class="card bg-primary">
@@ -134,7 +134,7 @@
 
     </div>
 
-    <div class="col-xl-4">
+    <div class="col-xl-4 col-xxl-2">
       <div class="container mt-5">
         <?php snippet('box-schnellzugriff') ?>
         <?php snippet('box-kalender') ?>
