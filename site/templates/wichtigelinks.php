b505008739154b3fb98a884de4ac6313a45c8c8a
Lennard <koala.panda@ewe.net>
2020-10-16 22:23:08 +0200

    Formulierungsänderung #40

--- site/templates/wichtigelinks.php
+++ site/templates/wichtigelinks.php
@@ -113,7 +113,7 @@
 
 <p>Hinweis: Artikel, die mit einem <svg class="bi" width="24" height="24">
     <use xlink:href="<?= $kirby->url('assets') ?>/icons/bootstrap-icons.svg#lightning-fill" />
-  </svg> markiert sind, sind direkt auf der Startseite markiert.
+  </svg> markiert sind, sind direkt auf der Startseite unter "Zur Zeit aktuelle Downloads" verlinkt.
 </p>
 
 
