4cf5dc7c89482b7711d79beb9a8f98f9813e469e
NilsPvR <nilsundgrk@gmx.de>
2020-12-01 16:48:41 +0100

    So funktioniert es

--- site/snippets/pagination.php
+++ site/snippets/pagination.php
@@ -1,4 +1,8 @@
-<?php if ($pagination->pages() > 1) : ?>
+<?php if ($pagination->hasPages()) : 
+$range = 4;
+$extraSeiten = 3;
+$gesamtSeitenzahl = $pagination->pages();
+$aktuelleSeite = $pagination->page() ?>
     <nav>
         <ul class="pagination flex-wrap justify-content-center">
 
@@ -8,22 +12,53 @@
              </li>           
             <?php endif ?>
 
+            
+            <?php for ($i = 1; $i <= $extraSeiten; $i++) : ?> 
+                <?php if ($i < ( $aktuelleSeite - ($range/2) + 0.5 )) : ?>
+
+                    <li class="page-item">
+                        <a class="page-link" href="<?= $pagination->pageURL($i) ?>">
+                            <?= $i ?>
+                        </a>
+                    </li>
+
+                <?php endif ?>               
+            <?php endfor ?>
+
             <!-- Hier nun die mittleren Elemente -->
 
-            <?php foreach ($pagination->range(10) as $r) : ?>
-                <li class="page-item <?= $pagination->page() === $r ? 'active' : '' ?>">
-                    <!-- Hier steckt hinter:
+            <?php foreach ($pagination->range($range) as $r) : 
+            //Es folgen die ersten x Elemente der collection 
+            ?>
+                
+                
+                    <li class="page-item <?= $aktuelleSeite === $r ? 'active' : '' ?>">
+                        <a class="page-link" <?= $aktuelleSeite === $r ? 'aria-current="page"' : '' ?> href="<?= $pagination->pageURL($r) ?>">
+                            <?= $r ?>
+                        </a>
+                    </li>
+                
+            <?php endforeach 
+            /* Hier steckt hinter:
             Wenn page() identisch ist mit $r schreibe aria-current (also aktuelle Seite),
             ansonsten mache '', also füge nichts ein. Das ist eine extreme Abkürzung.
 
             Damit wird im Endeffekt erreicht, dass mit nur einer Zeile die aktuelle Seite
-            markiert wird
-        -->
-                    <a class="page-link" <?= $pagination->page() === $r ? 'aria-current="page"' : '' ?> href="<?= $pagination->pageURL($r) ?>">
-                        <?= $r ?>
-                    </a>
-                </li>
-            <?php endforeach ?>
+            markiert wird */
+            ?>
+
+            <?php 
+            for ($i = $gesamtSeitenzahl - ($extraSeiten-1); $i <= $gesamtSeitenzahl; $i++) : ?> 
+                <?php if ($i > ($aktuelleSeite + $range/2)) : ?>
+
+                    <li class="page-item">
+                        <a class="page-link" href="<?= $pagination->pageURL($i) ?>">
+                            <?= $i ?>
+                        </a>
+                    </li>
+
+                <?php endif ?>               
+            <?php endfor ?>
 
             <?php if ($pagination->hasNextPage()) : ?>
                 <li class="page-item">
