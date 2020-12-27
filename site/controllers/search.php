075c61fd0fe91aaa53d370faf09c95641b500eea
Jesus_ <koala.panda@ewe.net>
2020-12-27 20:02:51 +0100

    sidewide search + ispublished?

--- site/controllers/search.php
+++ site/controllers/search.php
@@ -3,7 +3,8 @@
 return function ($site) {
 
   $query   = get('q');
-  $results = page('blogs')->search($query, 'title|text');
+  $sidewide = pages(["blogs", "kontakte", "Faecher", "allgemeines", "foerderverein", "formulare", "schule", "ser", "sv", "unterricht"])->children()->published();
+  $results = $sidewide->search($query, 'title|text');
   $results = $results->paginate(10);
 
   return [
