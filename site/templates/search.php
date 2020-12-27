69e78e331792190bb94ac39794dc7ff5e8783425
Jesus_ <koala.panda@ewe.net>
2020-12-27 13:24:12 +0100

    Autscrolling to results

--- site/templates/search.php
+++ site/templates/search.php
@@ -2,13 +2,14 @@
 
 <?php snippet('page-header') ?>
 
+<span id="top"></span> <!-- Autscrolling -->
 
 <form>
   <div class="form-floating input-group-lg mb-3 mx-3 d-block">
     <input type="search" class="form-control" id="floatingInput" placeholder="Testsuche" name="q" value="<?= html($query) ?>"> <!-- input-group-text -->
     <label for="floatingInput">Bitte geben Sie hier einen Suchbegriff ein:</label>
     <div class="d-grid gap-2 col-6 mx-auto mt-3">
-      <button class="btn btn-outline-secondary flex-nowrap" type="submit">Auf gut Glück!🔍</button>
+      <button class="btn btn-outline-secondary flex-nowrap" type="submit" formaction="#top">Auf gut Glück!🔍</button>
     </div>
   </div>
 </form>
