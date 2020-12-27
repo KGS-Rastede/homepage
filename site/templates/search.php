d09a7cc9050575df84aa347150940e9a18e43736
Carsten Niehaus <ni@kgs-rastede.de>
2020-12-27 15:11:11 +0100

    Bootstraplogo

--- site/templates/search.php
+++ site/templates/search.php
@@ -9,29 +9,33 @@
     <input type="search" class="form-control form-control-lg " id="floatingInput" placeholder="Testsuche" name="q" value="<?= html($query) ?>"> <!-- input-group-text -->
     <label for="floatingInput">Suchbegriff eingeben:</label>
     <div class="d-grid gap-2 col-6 mx-auto mt-3">
-      <button class="btn btn-outline-secondary flex-nowrap fs-5" type="submit" formaction="#top">Auf gut Glück!🔍</button>
+      <button class="btn btn-outline-secondary flex-nowrap fs-5" type="submit" formaction="#top">
+        <svg class="bi" width="24" height="24">
+          <use xlink:href="<?= $kirby->url('assets') ?>/icons/bootstrap-icons.svg#search" />
+        </svg> Los
+      </button>
     </div>
   </div>
 </form>
 
 <div class="m-3 mt-5">
   <ul>
-    <?php if($results->isNotEmpty()) : ?>
+    <?php if ($results->isNotEmpty()) : ?>
       <?php foreach ($results as $result) {
-        snippet('blogkarte', ['subpage' => $result]);  
-      }?>
-    <?php else: ?>
+        snippet('blogkarte', ['subpage' => $result]);
+      } ?>
+    <?php else : ?>
       <p>Es wurden leider keine Ergebnisse für "<?= html($query) ?>" gefunden.</p>
     <?php endif ?>
   </ul>
 
   <div class="d-flex justify-content-center">
     <?php
-      $pagination = $results->pagination();
+    $pagination = $results->pagination();
 
-      snippet('pagination', [
-        'pagination' => $pagination
-      ])
+    snippet('pagination', [
+      'pagination' => $pagination
+    ])
     ?>
   </div>
 </div>
