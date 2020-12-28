ded6b5eea91ef81c91c26f6a7b1aae541b05d937
Carsten Niehaus <ni@kgs-rastede.de>
2020-12-28 11:12:47 +0100

    #122 Auf dem echten Server ist ab sofort 2fa aktiv

--- site/config/config.kgs-rastede.de.php
+++ site/config/config.kgs-rastede.de.php
@@ -1,15 +1,26 @@
 <?php
 
 return [
-    // Damit bei Fehlern der Debugger angeht
-    // https://getkirby.com/docs/cookbook/setup/debugging-basics
+  // Damit bei Fehlern der Debugger angeht
+  // https://getkirby.com/docs/cookbook/setup/debugging-basics
 
-    // Auf dem echten Server muss der Debug-Modus aber aus sein! Das ist 
-    // eine potentielle Sicherheitslücke:
-    // https://getkirby.com/docs/guide/configuration#multi-enviroment-setup
-    'debug'  => false,
+  // Auf dem echten Server muss der Debug-Modus aber aus sein! Das ist 
+  // eine potentielle Sicherheitslücke:
+  // https://getkirby.com/docs/guide/configuration#multi-enviroment-setup
+  'debug'  => false,
 
-    'panel' => [
-        'slug' => 'intern'
-      ]
+  // 2fa aktivieren. Zuerst Passwort und dann den Code eingeben
+  'auth' => [
+    'methods' => [
+      'password' => [
+        '2fa' => true
+      ],
+      'code'
+    ]
+  ],
+
+
+  'panel' => [
+    'slug' => 'intern'
+  ]
 ];
