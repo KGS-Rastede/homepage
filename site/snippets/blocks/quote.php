d68d80d3c1b8f1e7ed7c2b879c79151865a5c73f
Carsten Niehaus <ni@kgs-rastede.de>
2021-03-13 16:37:16 +0100

    Oops... https://getbootstrap.com/docs/5.0/utilities/text/#line-height

--- site/snippets/blocks/quote.php
+++ site/snippets/blocks/quote.php
@@ -1,6 +1,6 @@
 <figure class="text-center">
     <div class="kgs-transitions zoom">
-        <p class="font-monospace lg-lg">&bdquo;<?= $block->text() ?>&ldquo;</p>
+        <p class="font-monospace lh-lg">&bdquo;<?= $block->text() ?>&ldquo;</p>
     </div>
     <figcaption class="blockquote-footer">
         <?= $block->citation() ?>
