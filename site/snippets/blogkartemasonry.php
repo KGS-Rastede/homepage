4827e11720eba1319ef45c140732a9d451cd6137
Nils van Rijsinge <nilsundgrk@gmx.de>
2021-03-28 08:57:06 +0000

    Mehr Blogkarten mit Bildern auf der Startseite

--- site/snippets/blogkartemasonry.php
+++ site/snippets/blogkartemasonry.php
@@ -1,9 +1,9 @@
 <div class="col-sm-6 col-lg-4 mb-4">
 
-    <?php if ($image = $subpage->gallery()->toFile()) : ?>
-
+    <?php if ($image = $subpage->gallery()->toFile()): ?>
+         
         <div class="card mb-3">
-            <img src="<?= $image->url() ?>" class="card-img-top" alt="">
+            <img src="<?= $image->url() ?>" class="card-img-top" alt="<?= $image->alt() ?>"  >
 
             <div class="fs-3 mt-3 fw-bold ps-3 text-danger">
                 <?= $subpage->title() ?>
@@ -21,6 +21,31 @@
                 </p>
             </div>
         </div>
+
+
+    <?php elseif ($block = $subpage->text()->toBlocks()->filterBy("type", "==", "image")->first()) : ?>
+        <?php $image = $block->image()->toFile() ?>
+
+        <div class="card mb-3">
+            <img src="<?= $image->url() ?>" class="card-img-top" alt="<?= $block->alt()->or($image->alt()) ?>"  >
+
+            <div class="fs-3 mt-3 fw-bold ps-3 text-danger">
+                <?= $subpage->title() ?>
+            </div>
+
+            <div class="card-body">
+                <p class="card-text">
+                    <?= $subpage->Text()->toBlocks()->excerpt(250) ?>
+                    <?php if ($subpage->author()->isNotEmpty()) : ?>
+                        (<?= $subpage->author() ?>)
+                    <?php endif ?>
+                </p>
+                <p class="text-end">
+                    <a class="text-decoration-none stretched-link" href="<?= $subpage->url() ?>">weiterlesen...</a>
+                </p>
+            </div>
+        </div>
+
 
     <?php else : ?>
 
