ebadcd77f05191764ba372d99e932fd0799f0d8e
Carsten Niehaus <ni@kgs-rastede.de>
2021-04-15 14:01:13 +0200

    vernünftige Tabelle

--- site/snippets/box-corona.php
+++ site/snippets/box-corona.php
@@ -1,34 +1,47 @@
-<div class="card mb-1">
+<div class="card mb-1 mt-4">
     <div class="card-body ">
-        <h4 class="card-category">Wetterstation</h4>
-        <h7>... unterstützt vom Förderverein</h7>
         <div class="text-start">
-           
+
 
             <div class="table-responsive">
+                Aktuelle Informationen zu Infektionsgeschehen an unserer Schule.
 
-            <table class="table">
-                <tr>
-                    <th>Messzeitpunkt</th>
-                </tr>
-                <tr>
-                    <td>moin</td>
-                </tr>
-               
-                <tr>
-                    <td><b>
-                            <center>Vorhersage</center>
-                        </b></td>
-                </tr>
-                <tr>
-                    <td>
-                        <center>sdfsdf</center>
-                    </td>
-                </tr>
-                <tr>
-                    <td>moin2</td>
-                </tr>
-            </table>
+                Derzeit befinden sich in vom Gesundheitamt angeordneter Quarantäne:
+
+                <table class="table">
+                    <!-- <
+                        thead>
+                        <tr>
+                            <th scope="col">In Quarantäne</th>
+                            <th scope="col">Schüler:innen Feldbreite</th>
+                            <th scope="col">Schüerl:innen Hauptgebäude</th>
+                            <th scope="col">Lehrkräfte</th>
+                        </tr>
+                    </thead> 
+                    -->
+                    <tbody>
+                        <tr>
+                            <th scope="row">Ganze Klasse</th>
+                            <td>3</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">Schüler:innen Feldbreite</th>
+                            <td>23</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">Schüerl:innen Hauptgebäude</th>
+                            <td>12</td>
+
+                        </tr>
+                        <tr>
+                            <th scope="row">Lehrkräfte</th>
+                            <td>12</td>
+
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
