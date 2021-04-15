58f4eb0a42745b59ff017f1b85ee83a06d741a8a
Carsten Niehaus <ni@kgs-rastede.de>
2021-04-15 13:52:31 +0200

    Coronatests

---
+++ site/snippets/box-corona.php
@@ -0,0 +1,35 @@
+<div class="card mb-1">
+    <div class="card-body ">
+        <h4 class="card-category">Wetterstation</h4>
+        <h7>... unterstützt vom Förderverein</h7>
+        <div class="text-start">
+           
+
+            <div class="table-responsive">
+
+            <table class="table">
+                <tr>
+                    <th>Messzeitpunkt</th>
+                </tr>
+                <tr>
+                    <td>moin</td>
+                </tr>
+               
+                <tr>
+                    <td><b>
+                            <center>Vorhersage</center>
+                        </b></td>
+                </tr>
+                <tr>
+                    <td>
+                        <center>sdfsdf</center>
+                    </td>
+                </tr>
+                <tr>
+                    <td>moin2</td>
+                </tr>
+            </table>
+            </div>
+        </div>
+    </div>
+</div>
