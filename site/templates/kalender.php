e91f0a91d94e04f1ce26f78a2b909092bab7aa06
Carsten Niehaus <ni@kgs-rastede.de>
2021-05-01 19:32:40 +0200

    Der Kalender will nicht

--- site/templates/kalender.php
+++ site/templates/kalender.php
@@ -7,7 +7,12 @@
 */ ?>
 
 <script src='https://cdn.jsdelivr.net/npm/fullcalendar/main.min.js'></script>
+<script src='https://github.com/mozilla-comm/ical.js/releases/download/v1.4.0/ical.js'></script>
+<script src='https://cdn.jsdelivr.net/npm/@fullcalendar/icalendar@5.6.0/main.global.js'></script>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fullcalendar/main.min.css" />
+
+
+
 
 
 <script>
@@ -21,48 +26,55 @@
         center: 'title',
         right: 'dayGridMonth,timeGridWeek,timeGridDay'
       },
-
       buttonText: {
         today: 'Heute',
         month: 'Monat',
         week: 'Woche',
         day: 'Tag',
       },
-
       // Samstag und Sonntag verstecken
-      hiddenDays: [0, 6], 
+      hiddenDays: [0, 6],
       firstDay: 1,
       navLinks: true, // can click day/week names to navigate views
       editable: false,
-      handleWindowResize: true, 
+      handleWindowResize: true,
 
       //Zeigt eine rote Linie an, die die aktuelle Zeit darstellt
-      nowIndicator: true, 
+      nowIndicator: true,
 
       //Die Stunden von 20 Uhr bis 07 Uhr morgens müssen nicht
       //angezeigt werden, dahier nie schulische Veranstaltungen sind
-      slotMinTime: "07:00:00",  
-      slotMaxTime: "20:00:00",  
+      slotMinTime: "07:00:00",
+      slotMaxTime: "20:00:00",
 
       // Kalenderwoche anzeigen
-      weekNumbers: true, 
+      weekNumbers: true,
 
       allDayText: "ganztägig",
-      
-      noEventsContent: 'Keine Ereignisse anzuzeigen',
-      
-      displayEventTime: false, // don't show the time column in list view
 
-    // Die Datei laden. Aktuell muss diese Datei noch per Hand generiert werden
-      events: '<?= $kirby->url('assets') ?>/kalender/schuljahresplaner.json'
+      noEventsContent: 'Keine Ereignisse anzuzeigen',
+
+      displayEventTime: false, // don't show the time column in list view
+      events: {
+        /*url: 'https://kgs-rastede.eu/iserv/public/calendar/?key=1bad7fec91a3d07904543f4476774fd1',*/
+        /*url: 'https://kgs-rastede.de/assets/kalender/public.ics',*/
+        /*url: 'ics/public.ics',*/
+        url: 'https://www.kip-ol.de/_kalender/_fullcalendar/examples/ics/public.ics',
+        format: 'ics',
+        failure: function() {
+          document.getElementById('script-warning').style.display = 'block';
+        }
+      },
+      loading: function(bool) {
+        document.getElementById('loading').style.display =
+          bool ? 'block' : 'none';
+      }
     });
 
     calendar.render();
-    calendar.refetchEvents(); //Test
   });
 </script>
 
-<div class="container-fluid">
-  <div id='calendar'></div>
-</div>
+<div id='calendar'></div>
+
 <?php snippet('footer') ?>
