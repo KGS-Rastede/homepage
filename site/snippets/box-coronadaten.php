2392a1fb512018b382dd8bc61bd9d4ac5c2049ad
Nils van Rijsinge <nilsundgrk@gmx.de>
2021-05-10 14:19:40 +0000

    Corona Zahlen Verarbeitung überarbeitet

--- site/snippets/box-coronadaten.php
+++ site/snippets/box-coronadaten.php
@@ -5,24 +5,42 @@
 
 snippet('src/rki_numbers');
 
+// <<<--- Inzidenzen aufrufen --->>>
 $id = 50; //Ammerland
-$cache_file = 'assets/rki_numbers_local.json';
-$incidence_local = new Incidence_local($id, $cache_file);
+$cache_file_local = 'assets/rki_numbers_local.json';
+$incidence_local = new Incidence_local($id, $cache_file_local);
 
 $today_local = $incidence_local->getDaily(0);
 
 $id = 0; //Deutschland
 $cache_file = 'assets/rki_numbers_brd.json';
-$incidence_brd = new Incidence_brd($id, $cache_file);
+$incidence_brd = new Incidence_brd($id, $cache_file, $cache_file_local);
 
 $today_brd = $incidence_brd->getDaily(0);
 
-$name_landkreis =  $today_local['GEN'];
-$incidence_landkreis = round($today_local['cases7_per_100k'], 2);
-$name_bundesland = $today_local['BL'];
-$incidence_bundesland = round($today_local['cases7_bl_per_100k'], 2);
-$incidence_brd =  round($today_brd['Inz7T'], 2);
-$datum_daten =  $today_local['last_update'];
+// <<<--- Variablen festlegen --->>>
+$loaded = false;
+if (isset($today_local) && isset($today_brd)) { //Wenn die Daten nicht leer sind
+  $name_landkreis =  $today_local['GEN'];
+  $incidence_landkreis = round($today_local['cases7_per_100k'], 2);
+  $name_bundesland = $today_local['BL'];
+  $incidence_bundesland = round($today_local['cases7_bl_per_100k'], 2);
+  $incidence_brd =  round($today_brd['Inz7T'], 2);
+  $datum_daten =  str_replace(', 00:00 Uhr', '', $today_local['last_update'], );
+  $loaded = true;
+}
+
+// <<<--- Festellen, ob Daten aktuell --->>>
+$day = new DateTime("today");
+$date_key = $day->format('Ymd');
+
+$date_numbers = DateTime::createFromFormat("d.m.Y, H:i", str_replace(" Uhr", "", $today_local['last_update']));
+$numbers_key = $date_numbers->format("Ymd");
+
+if ($date_key == $numbers_key) 
+  $uptodate = true;
+else
+  $uptodate = false;
 
 ?>
 
@@ -55,22 +73,32 @@ $datum_daten =  $today_local['last_update'];
 
             <td colspan="3" class="fs-3 text-center">Inzidenzen</td>
 
+              <?php if ($loaded) : //Wenn die Daten nicht leer sind ?> 
+                <tr>
+                  <th>
+                    <?= $name_landkreis . ": " . $incidence_landkreis ?>
+                  </th>
+                  <th>
+                    <?= $name_bundesland . ": " . $incidence_bundesland ?>
+                  </th>
+                  <th>
+                    <?= "Deutschland: " . $incidence_brd ?>
+                  </th>
+                </tr>
 
-              <tr>
+                <td colspan="3" class="fs-6">Inzidenzen (RKI, Stand: 
 
-                <th>
-                  <?= $name_landkreis . ": " . $incidence_landkreis ?>
-                </th>
-                <th>
-                  <?= $name_bundesland . ": " . $incidence_bundesland ?>
-                </th>
-                <th>
-                  <?= "Deutschland: " . $incidence_brd ?>
-                </th>
-              </tr>
+                  <?php if($uptodate) { //Wenn die Inzidenzen nicth aktuell sind soll das Datum rot sein
+                    echo $datum_daten . ')';
+                   }else {
+                    echo '<span class="text-danger">' . $datum_daten  . '</span>)';
+                   }
+                  ?>
 
-              <td colspan="3" class="fs-6">Inzidenzen (RKI, Stand: <?= $datum_daten ?>)</td>
-
+                </td>
+              <?php else : //Wenn die Daten leer sind ?>
+                <tr><th>Es konnten keine Inzidenzen geladen werden!</th></tr>
+              <?php endif ?>
             </tbody>
           </table>
         </td>
