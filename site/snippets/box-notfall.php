ed3aa614b3a8bf6f16e04bbbedb51eac50d60459
Carsten Niehaus <ni@kgs-rastede.eu>
2021-09-11 10:53:59 +0200

    korrekte Farbe nutzen

--- site/snippets/box-notfall.php
+++ site/snippets/box-notfall.php
@@ -5,7 +5,7 @@
     */
 ?>
 
-    <div class="text-white p-3 fw-light bg-dark">
+    <div class="bg-danger text-white p-3 fw-light">
         <h1><?php page('wichtige_informationen/')->textTitel() ?></h1>
         <p class="link-white"><?= page('wichtige_informationen/')->text()->kt() ?></p>
     </div>
