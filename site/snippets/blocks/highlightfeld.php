054de5e3769b0018868c675da031a8b75a6e0e42
Carsten Niehaus <ni@kgs-rastede.eu>
2021-10-02 10:52:16 +0200

    Purging verhindern

--- site/snippets/blocks/highlightfeld.php
+++ site/snippets/blocks/highlightfeld.php
@@ -1,3 +1,9 @@
+<!-- 
+Es folgen einige CSS-Klassen um sicherzustellen, dass diese nicht aus dem CSS gepurged werden
+bs-callout-primary bs-callout-secondary bs-callout-info bs-callout-warning bs-callout-danger bs-callout-light bs-callout-dark
+-->
+
+
 <div class="bs-callout bs-callout-info bg-white">
   <h4><?= $block->ueberschrift() ?></h4>
   <?= $block->feldinhalt()->kt() ?>
