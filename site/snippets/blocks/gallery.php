24549b789760045db9dcc9046cd1d2d93d89504e
Carsten Niehaus <ni@kgs-rastede.eu>
2021-10-26 14:37:14 +0200

    Masonry-Layout geht. Gibt noch immer keinen Designpreis, aber schon viel besser

--- site/snippets/blocks/gallery.php
+++ site/snippets/blocks/gallery.php
@@ -3,6 +3,13 @@
 /** @var \Kirby\Cms\Block $block */ ?>
 
 
-<?php foreach ($block->images()->toFiles() as $image) : ?>
-    <img src="<?= $image->url() ?>" class="rounded mx-auto d-block" alt="<?= $image->beschreibung() ?>">
-<?php endforeach ?>
+<div class="row" data-masonry='{"percentPosition": true }'>
+
+    <?php foreach ($block->images()->toFiles() as $image) : ?>
+        <div class="col-sm-6 col-lg-4 mb-4">
+            <img src="<?= $image->url() ?>" class="rounded img-fluid" alt="<?= $image->beschreibung() ?>">
+        </div>
+    <?php endforeach ?>
+</div>
+
+<script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script>
