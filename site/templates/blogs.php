f261b096637d7ea27a820a461f6d068d2604dc46
Carsten Niehaus <ni@kgs-rastede.de>
2021-10-27 10:26:21 +0200

    Den Code in eine .js-Datei gelegt, damit er nicht doppelt vorliegt

--- site/templates/blogs.php
+++ site/templates/blogs.php
@@ -23,25 +23,7 @@
 </div>
 
 <script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script>
-<script>
-  //Das masonry layout wird nach dem die Seite vollständig geladen ist einmal neugemacht,
-    //sodass sich keine Bilder (die erst später laden und somit die Größe der Blogs verändern) überlappen
-  "use strict"
 
-  //das HTML Element erhalten in dem das masonry angewendet werden soll
-  const elem = document.getElementById('masonry-element');
-
-  //Masonry definieren und optionen festlegen
-  const msnry = new Masonry( elem, {
-    //optionen
-    percentPosition: true
-  });
-
-  //wenn die Seite vollständig geladen ist
-  window.onload = function() {
-    //masonry einmal neu ausrichten
-    msnry.layout();
-  }
-</script>
+<?= js('assets/js/masonry_neu_rendern.js') ?>
 
 <?php snippet('footer') ?>
