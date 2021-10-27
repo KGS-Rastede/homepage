86af0e205809bb5a0ecbf27b6dd935d5c349bda7
Carsten Niehaus <ni@kgs-rastede.de>
2021-10-27 10:10:20 +0200

    Neu ausrichten, damit das alles sich nie überlagert

--- site/snippets/gallery.php
+++ site/snippets/gallery.php
@@ -1,4 +1,4 @@
-<div class="row">
+<div class="row" id="masonry-element">
   <?php foreach ($page->gallery()->toFiles() as $image) : ?>
 
     <div class="col-12 col-md-6 d-flex justify-content-center align-items-center">
@@ -12,3 +12,24 @@
         
   <?php endforeach ?>
 </div>
+
+<script>
+  //Das masonry layout wird nach dem die Seite vollständig geladen ist einmal neugemacht,
+    //sodass sich keine Bilder (die erst später laden und somit die Größe der Blogs verändern) überlappen
+  "use strict"
+
+  //das HTML Element erhalten in dem das masonry angewendet werden soll
+  const elem = document.getElementById('masonry-element');
+
+  //Masonry definieren und optionen festlegen
+  const msnry = new Masonry( elem, {
+    //optionen
+    percentPosition: true
+  });
+
+  //wenn die Seite vollständig geladen ist
+  window.onload = function() {
+    //masonry einmal neu ausrichten
+    msnry.layout();
+  }
+</script>
