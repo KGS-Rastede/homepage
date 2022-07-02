7d9d033bf325e1cf42e592b611b44ba237a0dbe9
Carsten Niehaus <gitlab@niehaus-ol.de>
2022-07-02 13:09:01 +0200

    Linien werden gezeichnet, da ist aber ein Bug: Die Schleife ist leer

--- site/snippets/blocks/reiseroute.php
+++ site/snippets/blocks/reiseroute.php
@@ -202,4 +202,81 @@
     map.getCanvas().style.cursor = '';
     popup.remove();
   });
+
+  map.on('load', () => {
+    map.addSource('route', {
+      'type': 'geojson',
+      'data': {
+        'type': 'Feature',
+        'properties': {},
+        'geometry': {
+          'type': 'LineString',
+          'coordinates': [
+
+            //forschleife fängt an
+
+            //anders als oben ist die Schleife leer, es werden keine Iterationen gemacht
+            // Ich verstehe den Fehler nicht
+
+            <?php foreach ($block->reise()->toBlocks() as $block) : ?> {
+                [<?= $block->breitengrad() ?>, <?= $block->laengengrad() ?>],
+              }
+            <?php endforeach ?>
+
+
+            //for endet
+
+
+            //zweites for hört auf
+
+            //das hier sind die richtigen Koordinaten
+
+         //   [139.79, 35.7],
+          //  [128.05, 36.26],
+          //  [120, 23.6],
+
+
+            [-122.483696, 37.833818],
+            [-122.483482, 37.833174],
+            [-122.483396, 37.8327],
+            [-122.483568, 37.832056],
+            [-122.48404, 37.831141],
+            [-122.48404, 37.830497],
+            [-122.483482, 37.82992],
+            [-122.483568, 37.829548],
+            [-122.48507, 37.829446],
+            [-122.4861, 37.828802],
+            [-122.486958, 37.82931],
+            [-122.487001, 37.830802],
+            [-122.487516, 37.831683],
+            [-122.488031, 37.832158],
+            [-122.488889, 37.832971],
+            [-122.489876, 37.832632],
+            [-122.490434, 37.832937],
+            [-122.49125, 37.832429],
+            [-122.491636, 37.832564],
+            [-122.492237, 37.833378],
+            [-122.493782, 37.833683]
+          ]
+
+
+
+        }
+      }
+    });
+    map.addLayer({
+      'id': 'route',
+      'type': 'line',
+      'source': 'route',
+      'layout': {
+        'line-join': 'round',
+        'line-cap': 'round'
+      },
+      //Linien Farbe und Stärke festlegen
+      'paint': {
+        'line-color': '#00ff00',
+        'line-width': 4
+      }
+    });
+  });
 </script>
