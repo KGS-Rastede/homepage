51c6c48c5b97fb83a03389cb3349523675a977d9
Carsten Niehaus <gitlab@niehaus-ol.de>
2023-03-31 10:06:01 +0200

    Das Bannerbild kann nun direkt festgelegt werden

--- site/snippets/home-page-header.php
+++ site/snippets/home-page-header.php
@@ -3,8 +3,46 @@
   <!-- The CSS grid that makes up the entirety of the hero image/banner image area -->
   <section class="top-banner-section">
     <!-- The CSS grid area that displays the image (layer 1) -->
+
+    <?php
+    // Code nach https://www.php.net/manual/en/function.date-sun-info.php
+
+    // Die Bilder sind in /content/design/ gespeichert
+    $page = page("design");
+
+    // Dieser Pfad speichert die URL des Hintergrundbildes
+    $bannerpfad = $page->bildregen()->toFile()->url();
+
+
+    // Setze die Zeitzone auf Berlin
+    date_default_timezone_set('Europe/Berlin');
+
+    // Setze deine Breiten- und Längengrade hier
+    $lat = 53.25; // Rastede
+    $long = 8.215;
+
+    // Hole die Sonneninformationen für heute
+    $sun_info = date_sun_info(time(), $lat, $long);
+    
+    // Hole die aktuelle Zeit als Timestamp
+    $now = time();
+    
+    // Bestimme das Bild basierend auf der Zeit
+    if ($now >= $sun_info['sunrise'] && $now < $sun_info['transit']) {
+      // Von Sonnenaufgang bis Zenit
+      $bannerpfad = $page->bildmorgens()->toFile()->url();
+    } elseif ($now >= $sun_info['transit'] && $now < $sun_info['sunset']) {
+      // Von Zenit bis Sonnenuntergang
+      $bannerpfad = $page->bildtag()->toFile()->url();
+    } else {
+      // Von Sonnenuntergang bis Sonnenaufgang des Folgetages
+      $bannerpfad = $page->bildnacht()->toFile()->url();
+    }
+    
+    ?>
+
     <div class="banner-image-div">
-      <img class="banner-image" src="<?= $kirby->url('assets') ?>/img/IMG_1343.jpeg" alt="Banner Image" />
+      <img class="banner-image" src="<?= $bannerpfad ?>" alt="Banner Image" />
     </div>
     <!-- The CSS grid area that displays the semi-transparent gradient overlay (layer 2) -->
     <div class="banner-overlay-div"></div>
