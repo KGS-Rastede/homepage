b237c1b0dfa2e66897430a65f5dd238781ff8d83
Carsten Niehaus <gitlab@niehaus-ol.de>
2023-07-23 11:40:25 +0200

    code auräumen

--- site/templates/elterninfos.php
+++ site/templates/elterninfos.php
@@ -7,7 +7,7 @@
 <div class="container">
   <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 justify-content-center">
 
-    <?php foreach ($page->text()->toBlocks() as $block): ?>
+    <?php foreach ($page->text()->toBlocks() as $block) : ?>
 
       <div class="card text-bg-secondary mb-3" style="max-width: 18rem;">
         <div class="card-header">
@@ -19,11 +19,20 @@
           </h5>
           <p class="card-text">
             <?= $block->kurzbeschreibung()->kt() ?>
-
-            <a href="<?= $block->link() ?>">Weiterlesen</a>
-
-
           </p>
+
+
+
+          <?php if ($block->link()->toFile() || $block->link()->toPage()) : ?>
+            <p><a href="<?= $block->link()->permalink() ?>">Öffnen...</a></p>
+          <?php else : ?>
+            <p><a href="<?= $block->link()->url() ?>">Webseite öffnen…</a></p>
+          <?php endif; ?>
+
+
+
+
+
         </div>
       </div>
 
