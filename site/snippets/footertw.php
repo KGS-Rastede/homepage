4a36bd528244098a095c27f4855483c162f545be
Carsten Niehaus <gitlab@niehaus-ol.de>
2023-12-04 18:27:20 +0100

    padding etwas kleiner, minimal weniger CSS durch den Footer

--- site/snippets/footertw.php
+++ site/snippets/footertw.php
@@ -62,7 +62,7 @@
         </div>
 
         <!--Copyright section-->
-        <div class="bg-neutral-200 p-4 text-center text-neutral-700 dark:bg-neutral-700 dark:text-neutral-200">
+        <div class="bg-neutral-200 py-2 text-center text-neutral-700 dark:bg-neutral-700 dark:text-neutral-200">
 
             <div class="col" id="copyrightText">
             </div>
