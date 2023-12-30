32ff7c17a21ef8db0e1948d8c842ebfb602ded13
Carsten Niehaus <gitlab@niehaus-ol.de>
2023-12-30 15:37:49 +0100

    fach.php hat nun eine TailwindCSS Tabelle

--- site/templates/fach.php
+++ site/templates/fach.php
@@ -6,26 +6,27 @@
 <div class="container">
   <?php if ($page->lehrplaene()->isNotEmpty()): ?>
 
-    <h2>Lehrpläne</h2>
+    <h2 class="text-2xl">Lehrpläne</h2>
 
-    <?= $page->Lehrplantext()->kirbytext() ?>
+    <div class="py-2">
+      <?= $page->Lehrplantext()->kirbytext() ?>
+    </div>
+    <div class="mt-8 flow-root">
+      <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+          <table class="min-w-full divide-y divide-gray-300">
 
-
-    <div class="row align-items-start">
-      <div class="col-xl-10">
-        <div class="table-responsive">
-          <table class="table">
             <thead>
               <tr>
-                <th>Lehrplan</th>
+                <th scope="col" class="px-3 py-3.5 text-left text-xl font-semibold text-gray-900">Lehrplan</th>
               </tr>
             </thead>
             <tbody>
 
               <?php foreach ($page->lehrplaene()->toFiles() as $lehrplan): ?>
                 <tr>
-                  <td>
-                    <a href="<?= $lehrplan->url() ?>" class="text-decoration-none">
+                  <td class="whitespace-nowrap px-3 py-4">
+                    <a href="<?= $lehrplan->url() ?>">
                       <?= $lehrplan->kurzbeschreibung()->or($lehrplan->name()) ?>
                     </a>
                   </td>
