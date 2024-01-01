c190ba566fc08713535eeac192036977d8679433
Carsten Niehaus <gitlab@niehaus-ol.de>
2024-01-01 14:15:18 +0100

    Und nochmal zwei Bootstrapklassen weniger :-)

--- site/templates/zweige.php
+++ site/templates/zweige.php
@@ -3,20 +3,27 @@
 
 <?php snippet('sidebar') ?>
 
-<div class="section-a">
-  <div class="container">
-    <div class="row">
-      <?php foreach ($page->children() as $zweig) : ?>
-        <div class="col-md-4">
-          <h3><?= $zweig->title() ?></h3>
-          <p>Leitung: <?= $zweig->leitung() ?></p>
-          <a href="<?= $zweig->url() ?>" class="btn btn-danger">
-            Weitere Informationen
-          </a>
-        </div>
-      <?php endforeach ?>
+<div class="container mx-auto p-4">
 
-    </div>
+  <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+
+    <?php foreach ($page->children() as $zweig): ?>
+      <div class="bg-white p-4 shadow-md">
+        <h1 class="mt-6 text-2xl font-semibold text-gray-900">
+          <?= $zweig->title() ?>
+        </h1>
+
+        <h3 class="my-8 block text-gray-900">
+          Leitung:
+          <?= $zweig->leitung() ?>
+
+        </h3>
+        <?php snippet('knopf-klein', ['subpage' => $zweig, 'knopftext' => "Weitere Informationen"]); ?>
+
+
+      </div>
+    <?php endforeach ?>
+
   </div>
 </div>
 
