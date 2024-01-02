0371b33300b67123d3888c7dfa19b98d196fcf46
Carsten Niehaus <gitlab@niehaus-ol.de>
2024-01-02 13:02:41 +0100

    Nutzerordnung nun nur noch TailwindCSS (nur 1 BS Klasse weniger)

--- site/templates/nutzerordnung.php
+++ site/templates/nutzerordnung.php
@@ -7,23 +7,30 @@
     <p>
         <?= $page->Beschreibung()->kirbytext() ?>
     </p>
-    <div class="row row-cols-1 row-cols-md-2 g-4">
-        <?php
-        $dateien =  $page->dateien()->toFiles();
-        foreach ($dateien as $datei) : ?>
-            <div class="col">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title"><?= $datei->anzeigename()->or($datei->name()) ?></h5>
-                        <p class="card-text"><?= $datei->beschreibung() ?></p>
 
-                        <?php snippet('knopf-klein', ['subpage' => $datei, 'knopftext' => "Datei herunterladen"]); ?>
+    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+        <div class="grid grid-cols-1 p-4 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
+
+            <?php
+            $dateien = $page->dateien()->toFiles();
+            foreach ($dateien as $datei): ?>
+                <div class="group  bg-slate-100 p-4">
+
+                    <div class="mb-4">
+                        <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                            <?= $datei->anzeigename()->or($datei->name()) ?>
+                        </h3>
+                        <p class="mt-5 line-clamp-3 leading-2 text-gray-600">
+                            <?= $datei->beschreibung() ?>
+                        </p>
 
                     </div>
-                </div>
-            </div>
+                    <?php snippet('knopf-klein', ['subpage' => $datei, 'knopftext' => "Datei herunterladen"]); ?>
 
-        <?php endforeach ?>
+                </div>
+
+            <?php endforeach ?>
+        </div>
     </div>
 
 </div>
