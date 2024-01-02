68eab9fe8e3372b1911c1e4c914e6bebb93d1cca
Carsten Niehaus <gitlab@niehaus-ol.de>
2024-01-02 14:03:10 +0100

    Kinderfriedhof komplett neu gemacht

--- site/templates/gedenktafel.php
+++ site/templates/gedenktafel.php
@@ -2,48 +2,33 @@
 
 <?php snippet('page-header') ?>
 
-<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="https://kgs-rastede.de/media/pages/blogs/festakt-zum-20jaehrigen-jubilaeum-schule-ohne-rassismus-schule-mit-courage/f4f5b72cf2-1655105183/festakt6.jpg" class="d-block w-50" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5> Moin moin
-        </h5>
-        <p>Some representative placeholder content for the first slide.</p>
-      </div>
-    </div>
-    <?php foreach ($page->text()->toBlocks() as $block) : ?>
+<div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+  <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
 
-      <div class="carousel-item ">
-        <?php echo $block->grabsteinbild()->toFile() ?>
-        <div class="carousel-caption d-none d-md-block">
-          <h5> <?php echo $block->beschriftung() ?>
-          </h5>
-          <p>
+    <?php foreach ($page->text()->toBlocks() as $block): ?>
+      <div class="group aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg xl:aspect-h-8 xl:aspect-w-7">
 
-            <?php echo $block->grabsteinbild()->toFile() ?>
+        <div class="mt-1">
+          <?php echo $block->grabsteinbild()->toFile() ?>
 
-            GebDatum <?php echo $block->gebdatum() ?> TodesDatum <?php echo $block->todesdatum() ?>
-
-          </p>
+          <h2 class="mt-3 text-2xl font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+            <?php echo $block->beschriftung() ?>
+          </h2>
+          <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+            geb.:
+            <?php echo $block->gebdatum() ?>
+          </h3>
+          <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+            gestorben:
+            <?php echo $block->todesdatum() ?>
+            </p>
         </div>
+
       </div>
 
     <?php endforeach ?>
+
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
 </div>
 
 <?php snippet('footertw') ?>
