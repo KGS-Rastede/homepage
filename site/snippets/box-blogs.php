9c1a6bce7b1da68c4dcc48170ae2417820aa8165
Carsten Niehaus <gitlab@niehaus-ol.de>
2024-01-17 13:02:00 +0100

    2-spaltig auf der Startseite (bessere Lesbarkeit) und das snippet wurde umbenannt.

--- site/snippets/box-blogs.php
+++ site/snippets/box-blogs.php
@@ -1,10 +1,10 @@
 <h2 class="font-semibold text-xl">Aus dem Schulleben</h2>
 
-<?php if (collection('blogs-startseite')->isNotEmpty()) : // Wenn aktuelle Artikel vorhanden sind  
-?>
-  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
-    <?php foreach (collection('blogs-startseite') as $subpage) : ?>
-      <?php snippet('blogkartemasonry', ['subpage' => $subpage]); ?>
+<?php if (collection('blogs-startseite')->isNotEmpty()): // Wenn aktuelle Artikel vorhanden sind  
+    ?>
+  <div class="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-2">
+    <?php foreach (collection('blogs-startseite') as $subpage): ?>
+      <?php snippet('blogkarte-bild', ['subpage' => $subpage]); ?>
     <?php endforeach; ?>
   </div>
 <?php endif ?>
@@ -12,11 +12,11 @@
 <div class="p-4">
 
   <a href="<?= page("blogs")->url() ?>">
-    <?php if (collection('blogs-startseite')->isNotEmpty()) : // Wenn aktuelle Artikel vorhanden sind  
-    ?>
+    <?php if (collection('blogs-startseite')->isNotEmpty()): // Wenn aktuelle Artikel vorhanden sind  
+        ?>
       <?php snippet('knopf-klein', ['subpage' => page("blogs"), 'knopftext' => "Weitere Nachrichten aus der Schule &#8594;"]); ?>
-    <?php else : // Wenn keine aktuellen Artikel vorhanden sind  
-    ?>
+    <?php else: // Wenn keine aktuellen Artikel vorhanden sind  
+        ?>
       <?php snippet('knopf-klein', ['subpage' => page("blogs"), 'knopftext' => "Nachrichten aus der Schule &#8594;"]); ?>
     <?php endif ?>
   </a>
