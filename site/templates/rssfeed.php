1fc5b7eb629b686a9e041a250b47a1200c104641
Ole <github@olebohmbach.eu>
2024-04-30 17:41:04 +0200

    RSS Feed #1

---
+++ site/templates/rssfeed.php
@@ -0,0 +1,12 @@
+<div class="feed">
+    <?php foreach ($page->children()->sortBy('date', 'desc') as $item): ?>
+      <article>
+        <header class="article-header">
+          <h2><a><?= $item->title() ?></a></h2>
+        </header>
+        <div class="article-body">
+          <?= $item->description()->kirbytext() ?>
+        </div>
+      </article>
+    <?php endforeach ?>
+</div>
