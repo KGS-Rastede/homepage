e06de91033e58649114c8b32cd4856f1a3eb39bd
Clown167 <mastercaker@web.de>
2024-08-19 09:54:10 +0200

    Raumnummern werden nun auch im 3D modell angezeigt (nachdem man in der 2D Ansicht war)

--- site/snippets/blocks/reiseroute.php
+++ site/snippets/blocks/reiseroute.php
@@ -142,7 +142,7 @@ echo ($features);
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v12',
         center: [8.1964, 53.2442],
-        zoom: 18,
+        zoom: 18.5,
         pitch: 50,
         bearing: 20,
         antialias: true,
@@ -159,6 +159,18 @@ echo ($features);
     //  Karte Laden
     // ---------------------------------------------------------
     map.on('load', () => {
+        //Die KGS-Rastede (Hauptgebäude dynamisch heranzoomen)
+        
+        map.fitBounds([
+            [8.1960, 53.2435], // [lng, lat] - southwestern corner of the bounds
+            [8.1968, 53.2446]  // [lng, lat] - northeastern corner of the bounds
+        ], {
+            //padding: {top: 10, bottom: 25, left: 15, right: 5}, // Optional padding
+            pitch: map.getPitch(),  // Behalte den aktuellen pitch
+            bearing: map.getBearing(), // Behalte den aktuellen bearing
+            maxZoom: map.getZoom() // Optional: Begrenze den Zoom, damit er nicht zu weit herauszoomt
+        });
+        
         /*
         ACHTUNG: Damit das funktioniert muss noch eine geeignete Quelle für die Wetterdaten gefunden werden
         Hier ist das genutzte Beispiel nur ein Bild. Siehe: https://docs.mapbox.com/mapbox-gl-js/example/image-on-a-map/
@@ -189,7 +201,6 @@ echo ($features);
             'type': 'geojson',
             'data': '/content/allgemeines/anfahrt/bereinigte_geojson_datei.geojson'
         });
-
 
 
         twoD = false;
@@ -304,14 +315,15 @@ echo ($features);
         addLayerForRooms('2');
 
         function addLayerRoomnumbers(level){
+            var offset = level*(-4)
             map.addLayer({
             'id': `room_labels_floor_${level}`,
             'type': 'symbol',
             'source': 'floorplan',
             'filter': ['==', 'level', level],
             'layout': {
-                'text-allow-overlap': true,  // The icon will be visible even if it collides with other previously drawn symbols.
-                'text-ignore-placement': true, 
+                'text-allow-overlap': false,  // The icon will be visible even if it collides with other previously drawn symbols.
+                'text-ignore-placement': false, 
                 'text-field': ['get', 'name'], 
                 'text-size': [
                     'interpolate',
@@ -325,7 +337,7 @@ echo ($features);
                     21, 50,   // zoomstufe , textgröße
                     22, 100   // zoomstufe , textgröße
                 ],
-                'text-anchor': 'center',
+                'text-offset': [0, offset] // Offset, um den Text zu verschieben, falls nötig
             },
             'paint': {
                 'text-color': '#fff',
@@ -340,6 +352,8 @@ echo ($features);
         addLayerRoomnumbers('0');
         addLayerRoomnumbers('1');
         addLayerRoomnumbers('2');
+
+        raumnummernVerstecken();
 
         function addWalls(level){
             map.addLayer({
@@ -356,6 +370,7 @@ echo ($features);
                 },
                 'paint': {
                 'line-color': '#222',
+                'line-opacity': 0.25,
                 'line-width': 1
                 }
             });
@@ -365,7 +380,6 @@ echo ($features);
         addWalls('1');
         addWalls('2');
 
-        raumnummernVerstecken();
 
     //==========================================================
     //==========Icons==========
@@ -435,20 +449,23 @@ echo ($features);
 
         //Höhe auf 0 setzen
         levels.forEach((level) => {
-                map.setPaintProperty(`floor_extrusion_${level}`, 'fill-extrusion-height', 0);
-                map.setPaintProperty(`floor_extrusion_${level}`, 'fill-extrusion-base', 0);
+            map.setPaintProperty(`floor_extrusion_${level}`, 'fill-extrusion-height', 0);
+            map.setPaintProperty(`floor_extrusion_${level}`, 'fill-extrusion-base', 0);
 
-                map.setPaintProperty(`hall_extrusion_${level}`, 'fill-extrusion-height', 0.01);
-                map.setPaintProperty(`hall_extrusion_${level}`, 'fill-extrusion-base', 0.01);
+            map.setPaintProperty(`hall_extrusion_${level}`, 'fill-extrusion-height', 0.01);
+            map.setPaintProperty(`hall_extrusion_${level}`, 'fill-extrusion-base', 0.01);
 
-                map.setPaintProperty(`room_extrusion_${level}`, 'fill-extrusion-height', 0.01);
-                map.setPaintProperty(`room_extrusion_${level}`, 'fill-extrusion-base', 0.01);
+            map.setPaintProperty(`room_extrusion_${level}`, 'fill-extrusion-height', 0.01);
+            map.setPaintProperty(`room_extrusion_${level}`, 'fill-extrusion-base', 0.01);
 
-                map.setPaintProperty(`stair_extrusion_${level}`, 'fill-extrusion-height', 0.01);
-                map.setPaintProperty(`stair_extrusion_${level}`, 'fill-extrusion-base', 0.01);
+            map.setPaintProperty(`stair_extrusion_${level}`, 'fill-extrusion-height', 0.01);
+            map.setPaintProperty(`stair_extrusion_${level}`, 'fill-extrusion-base', 0.01);
 
-                map.setPaintProperty(`room_searched`, 'fill-extrusion-height', 0.01);
-                map.setPaintProperty(`room_searched`, 'fill-extrusion-base', 0.01);
+            map.setLayoutProperty(`room_labels_floor_${level}`, 'text-offset', [0, 0.01]);
+
+            map.setPaintProperty(`room_searched`, 'fill-extrusion-height', 0.01);
+            map.setPaintProperty(`room_searched`, 'fill-extrusion-base', 0.01);
+                
         });
 
         toggleFloor(); 
@@ -462,10 +479,11 @@ echo ($features);
         map.touchZoomRotate.enableRotation(); //enable rotation
         map.easeTo({ pitch: 50, zoom: 18 }); // Set pitch to 50 degrees for 3D view and zoom in
     
-        raumnummernVerstecken();
+        //raumnummernVerstecken();
 
         //Höhe auf auf 3d setzen
         levels.forEach((level) => {
+            var offset = level*(-4);
             map.setPaintProperty(`floor_extrusion_${level}`, 'fill-extrusion-height', ['get', 'height']);
             map.setPaintProperty(`floor_extrusion_${level}`, 'fill-extrusion-base', ['get', 'base_height']);
 
@@ -477,6 +495,8 @@ echo ($features);
 
             map.setPaintProperty(`stair_extrusion_${level}`, 'fill-extrusion-height', ['get', 'height']);
             map.setPaintProperty(`stair_extrusion_${level}`, 'fill-extrusion-base', ['get', 'base_height']);
+
+            map.setLayoutProperty(`room_labels_floor_${level}`, 'text-offset', [0, offset]);
 
             map.setPaintProperty(`room_searched`, 'fill-extrusion-height', ['get', 'height']);
             map.setPaintProperty(`room_searched`, 'fill-extrusion-base', ['get', 'base_height']);
@@ -508,8 +528,9 @@ echo ($features);
     function toggleFloor() {
         raumnummernVerstecken();
         // Raumnummern für die ausgewählte Etage anzeigen
+        map.setLayoutProperty(`room_labels_floor_${etage}`, 'visibility', 'visible');
         if(twoD){
-            map.setLayoutProperty(`room_labels_floor_${etage}`, 'visibility', 'visible');
+            
             levels.forEach((level) => {
                 map.setLayoutProperty(`room_walls_${level}`, 'visibility', etage === level ? 'visible' : 'none');
                 map.setPaintProperty(`room_searched`, 'fill-extrusion-height', 0.01);
