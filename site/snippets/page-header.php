e429a52599908416da9f21da9fa3b5b77baec291
Carsten Niehaus <gitlab@niehaus-ol.de>
2025-04-16 12:29:17 +0200

    kleinere Farbanpassungen

--- site/snippets/page-header.php
+++ site/snippets/page-header.php
@@ -1,5 +1,5 @@
 <!-- Titelfeld -->
-<div class="bg-neutral-100">
+<div class="bg-neutral-200">
   <div class="container mx-auto max-w-7xl p-3 lg:px-8 mb-2">
     <h1 class="text-2xl font-semibold">
       <?= $page->title() ?>
