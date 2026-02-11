1cbc9d7a61a2610268f0d252281101bf9057fb12
Carsten Niehaus <gitlab@niehaus-ol.de>
2026-02-11 18:12:47 +0100

    Die Knöpfe da oben sind nun konfigurierbar

--- site/snippets/box-schnellverweise.php
+++ site/snippets/box-schnellverweise.php
@@ -1,18 +1,23 @@
+<?php
+$buttons = page('wichtige-informationen')->buttons()->toStructure();
+?>
+
 <div class="flex flex-col justify-center gap-1 md:flex-row md:gap-4 mx-2 sm:mx-6 lg:mx-12 my-6">
+    <?php foreach ($buttons as $btn): ?>
+        <?php
+        if ($btn->active()->toBool() === false) continue;
 
+        // 1) Bevorzugt: interne Seite
+        $subpage = $btn->page()->toPage(); // null, wenn nicht gesetzt
 
-    <?= snippet('knopf-gross', [
-        'subpage' => page('schule/oberstufe'),
-        'knopftext' => 'Information Oberstufe',
-    ]) ?>
+        // Button-Text
+        $text = $btn->text()->or('Weiter')->value();
+        ?>
 
-    <?= snippet('knopf-gross', [
-        'subpage' => page('blogs/anmeldung-5'),
-        'knopftext' => 'Anmeldung JG 5',
-    ]) ?>
+        <?= snippet('knopf-gross', [
+            'subpage'   => $subpage,    // Page|null
+            'knopftext' => $text       // string
+        ]) ?>
 
-    <?= snippet('knopf-gross', [
-        'subpage' => page('allgemeines/schulbuchlisten'),
-        'knopftext' => 'Schulbücher',
-    ]) ?>
+    <?php endforeach; ?>
 </div>
