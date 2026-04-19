0b111027c6c634c4df9f561d85ba1f3d79224d23
Carsten Niehaus <gitlab@niehaus-ol.de>
2026-04-19 16:38:52 +0200

    Layout korrigiert (nur in der relativen Mitte)

--- site/snippets/box-schnellverweise.php
+++ site/snippets/box-schnellverweise.php
@@ -2,7 +2,7 @@
 $buttons = page('wichtige-informationen')->buttons()->toStructure();
 ?>
 
-<div class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-3 mx-2 sm:mx-6 lg:mx-12 my-6">
+<div class="flex flex-wrap justify-center gap-3 mx-2 sm:mx-6 lg:mx-12 my-6">
     <?php foreach ($buttons as $btn): ?>
         <?php
         if ($btn->active()->toBool() === false) continue;
