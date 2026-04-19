63c884aff0625b9cb82e5ec557c1468199c58561
Carsten Niehaus <gitlab@niehaus-ol.de>
2026-04-19 16:00:42 +0200

    Code vereinfacht

--- site/snippets/box-links.php
+++ site/snippets/box-links.php
@@ -1,27 +1,21 @@
-<?php /* $links_bilder: Die URL zur Seite => Der Name des Bildes (im Ordner assets/bilder), $alt: Alternativtext für das Bild
- !!Beide arrays müssen gleich lang sein!! */
-$links_bilder = [
-  'https://kgs-rastede.webuntis.com/' => 'webuntis.png',
-  'https://kgs-rastede.eu' => 'IServ_Logo.svg',
-  'https://foerderverein.kgsraste.de/' => 'foerderverein.png',
-  'https://kgs-rastede.l-e-o.eu' => 'mensa.png',
-  'http://kgs-rastede.de/unterricht/erasmus' => 'eu.svg',
+<?php
+$links = [
+  ['url' => 'https://kgs-rastede.webuntis.com/', 'bild' => 'webuntis.png',     'alt' => 'Webuntis'],
+  ['url' => 'https://kgs-rastede.eu',             'bild' => 'IServ_Logo.svg',   'alt' => 'IServ'],
+  ['url' => 'https://foerderverein.kgsraste.de/', 'bild' => 'foerderverein.png','alt' => 'Förderverein'],
+  ['url' => 'https://kgs-rastede.l-e-o.eu',       'bild' => 'mensa.png',        'alt' => 'Mensa'],
+  ['url' => 'http://kgs-rastede.de/unterricht/erasmus', 'bild' => 'eu.svg',     'alt' => 'EU'],
 ];
-$alt = ['Webuntis', 'IServ', 'Förderverein', 'Mensa', 'EU'];
-
-$count = -1;
 ?>
 
 <div class="flex justify-center w-full mb-2 px-5 sm:px-0">
   <div class="grid grid-cols-1 max-w-7xl gap-2 sm:gap-4 md:grid-cols-6 md:gap-4 xl:gap-6 mx-auto">
-    <?php foreach ($links_bilder as $link => $bild):
-      $count++; ?>
-      <a href="<?= $link ?>" target="_blank" rel="noopener" class="flex justify-center items-center">
-        <img src="<?= $kirby->url('assets') ?>/bilder/<?= $bild ?>"
-          alt="<?= $alt[$count] ?>"
+    <?php foreach ($links as $eintrag): ?>
+      <a href="<?= $eintrag['url'] ?>" target="_blank" rel="noopener" class="flex justify-center items-center">
+        <img src="<?= $kirby->url('assets') ?>/bilder/<?= $eintrag['bild'] ?>"
+          alt="<?= $eintrag['alt'] ?>"
           class="max-h-16 md:max-h-32">
       </a>
-    <?php
-    endforeach; ?>
+    <?php endforeach; ?>
   </div>
 </div>
