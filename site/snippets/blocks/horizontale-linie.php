a376229ae901404a8d70cfc99c0f3c9ddf528cdf
Carsten Niehaus <gitlab@niehaus-ol.de>
2026-04-19 13:18:40 +0200

    Linie jetzt auch sichtbar

--- site/snippets/blocks/horizontale-linie.php
+++ site/snippets/blocks/horizontale-linie.php
@@ -1,4 +1,4 @@
-<div class="relative">
+<div class="relative h-px my-6">
     <div class="absolute inset-0 flex items-center" aria-hidden="true">
         <div class="w-full border-t border-slate-300"></div>
     </div>
