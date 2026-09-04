acf567ec0f661b78e39f2d4c30cf472d451d9544
Ole Bohmbach <mail@olebohmbach.eu>
2026-09-05 00:04:25 +0200

    Geschichtsseite: Timeline neu gestaltet

--- site/snippets/blocks/geschichtszeitraum.php
+++ site/snippets/blocks/geschichtszeitraum.php
@@ -1,88 +1,51 @@
-<!-- 
+<!--
+  Zeitraum-Abschnitt: kleiner Knoten (Jahreszahl) gefolgt von den Ereignissen
   site/blueprints/blocks/geschichtszeitraum.php
  -->
+<?php
+$ereignis = static function ($field): ?string {
+  $html =
+    preg_replace('/<p>\s*<\/p>/i', '', $field->value()) ?? $field->value();
 
+  if (trim(strip_tags($html)) === '') {
+    return null;
+  }
 
-<!-- Timeline: Centered Simple -->
-<div class="relative py-5 dark:text-slate-100">
-  <!-- Vertical Guide -->
-  <div
-    class="absolute top-0 bottom-0 left-0 flex w-10 flex-col justify-center md:w-12 lg:left-1/2 lg:-ml-6"
-    aria-hidden="true"
-  >
-    <div
-      class="mx-auto h-2.5 w-1 grow-0 rounded-t bg-linear-to-b from-transparent to-indigo-100 dark:to-indigo-900"
-    ></div>
-    <div class="mx-auto w-1 grow bg-indigo-100 dark:bg-indigo-900"></div>
-    <div
-      class="mx-auto h-2.5 w-1 grow-0 rounded-b bg-linear-to-t from-transparent to-indigo-100 dark:to-indigo-900"
-    ></div>
-  </div>
-  <!-- END Vertical Guide -->
+  return $html;
+};
 
-  <!-- Timeline -->
-  <ul class="relative space-y-4 pl-10 md:pl-12 lg:pl-0">
-    <?php if ($block->ereignis1()->isNotEmpty()): ?>
-  <?= snippet('geschichtsereignis-links', [
-    'ereignistext' => $block->ereignis1(),
-  ]) ?>
-<?php endif; ?>
+$events = [
+  ['feld' => 'ereignis1', 'seite' => 'links'],
+  ['feld' => 'ereignis2', 'seite' => 'rechts'],
+  ['feld' => 'ereignis3', 'seite' => 'links'],
+  ['feld' => 'ereignis4', 'seite' => 'rechts'],
+  ['feld' => 'ereignis5', 'seite' => 'links'],
+  ['feld' => 'ereignis6', 'seite' => 'rechts'],
+  ['feld' => 'ereignis7', 'seite' => 'links'],
+  ['feld' => 'ereignis8', 'seite' => 'rechts'],
+  ['feld' => 'ereignis9', 'seite' => 'links'],
+  ['feld' => 'ereignis10', 'seite' => 'rechts'],
+];
+?>
+<div class="relative">
+  <?php if ($block->zeitraum()->isNotEmpty()): ?>
+    <div class="py-5 pl-8 lg:flex lg:justify-center lg:py-8 lg:pl-0 lg:text-center">
+      <span
+        class="inline-block rounded-full border border-indigo-200 bg-white px-5 py-1.5 text-sm font-bold uppercase tracking-wider text-slate-900 shadow-sm dark:border-indigo-800 dark:bg-slate-900 dark:text-slate-100"
+      >
+        <?= $block->zeitraum() ?>
+      </span>
+    </div>
+  <?php endif; ?>
 
-<?php if ($block->ereignis2()->isNotEmpty()): ?>
-  <?= snippet('geschichtsereignis-rechts', [
-    'ereignistext' => $block->ereignis2(),
-  ]) ?>
-<?php endif; ?>
-
-<?php if ($block->ereignis3()->isNotEmpty()): ?>
-  <?= snippet('geschichtsereignis-links', [
-    'ereignistext' => $block->ereignis3(),
-  ]) ?>
-<?php endif; ?>
-
-<?php if ($block->ereignis4()->isNotEmpty()): ?>
-  <?= snippet('geschichtsereignis-rechts', [
-    'ereignistext' => $block->ereignis4(),
-  ]) ?>
-<?php endif; ?>
-
-<?php if ($block->ereignis5()->isNotEmpty()): ?>
-  <?= snippet('geschichtsereignis-links', [
-    'ereignistext' => $block->ereignis5(),
-  ]) ?>
-<?php endif; ?>
-
-<?php if ($block->ereignis6()->isNotEmpty()): ?>
-  <?= snippet('geschichtsereignis-rechts', [
-    'ereignistext' => $block->ereignis6(),
-  ]) ?>
-<?php endif; ?>
-
-<?php if ($block->ereignis7()->isNotEmpty()): ?>
-  <?= snippet('geschichtsereignis-links', [
-    'ereignistext' => $block->ereignis7(),
-  ]) ?>
-<?php endif; ?>
-
-<?php if ($block->ereignis8()->isNotEmpty()): ?>
-  <?= snippet('geschichtsereignis-rechts', [
-    'ereignistext' => $block->ereignis8(),
-  ]) ?>
-<?php endif; ?>
-
-<?php if ($block->ereignis9()->isNotEmpty()): ?>
-  <?= snippet('geschichtsereignis-links', [
-    'ereignistext' => $block->ereignis9(),
-  ]) ?>
-<?php endif; ?>
-
-<?php if ($block->ereignis10()->isNotEmpty()): ?>
-  <?= snippet('geschichtsereignis-rechts', [
-    'ereignistext' => $block->ereignis10(),
-  ]) ?>
-<?php endif; ?>
-
+  <ul class="relative space-y-6 lg:space-y-8">
+    <?php foreach ($events as $event): ?>
+      <?php $text = $ereignis($block->{$event['feld']}()); ?>
+      <?php if ($text !== null): ?>
+        <?= snippet('geschichtsereignis-' . $event['seite'], [
+          'ereignistext' => $text,
+        ]) ?>
+      <?php endif; ?>
+    <?php endforeach; ?>
   </ul>
-  <!-- END Timeline -->
 </div>
-<!-- END Timeline: Centered Simple -->
