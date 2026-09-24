7beecb65e306c650424bc600a3c1db8b50fdcd10
Carsten Niehaus <ni@kgs-rastede.eu>
2026-09-24 11:10:55 +0200

    verstecken wenn leer

--- site/snippets/menu/menu-category-column.php
+++ site/snippets/menu/menu-category-column.php
@@ -3,10 +3,12 @@
 verwendet werden kann. */
 ?>
 <div class="space-y-6 p-8">
+    <?php if (!empty($title)): ?>
     <h4
         class="font-semibold tracking-wider text-kgs-blue uppercase dark:text-kgs-lblue">
         <?= $title ?>
     </h4>
+    <?php endif ?>
     <nav class="flex flex-col gap-3">
         <?php foreach ($menuItems as $item) {
           snippet('menu/menu-item', $item);
